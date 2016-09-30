6b7b46516cb876e2c5568e8189b5dc661b448948
onedou <onedou@126.com>
2016-09-30 17:33:23 +0800

    update packages.page
    Signed-off-by: onedou <onedou@126.com>

--- www/wiki/controllers/packagesProjectsController.js
+++ www/wiki/controllers/packagesProjectsController.js
@@ -1,21 +1,99 @@
 angular.module('MyApp')
-.controller('packagesProjectsController', function ($scope, $uibModal, Account, $http) {
+.factory('packagesService', function () {
+    var newPackages = {
+        status: 0,
+        type: ''
+    };
+
+    return {
+        getNewPackages: function () {
+            return newPackages;
+        },
+        setNewPackages: function (_newPackages) {
+            newPackages = _newPackages;
+        }
+    };
+})
+.controller('packagesProjectsController', function ($scope, $uibModal, $http, Account, packagesService) {
     Account.setRequireSignin(true);
 
-    $http({
-        method: 'POST',
-        url: '/api/wiki/models/packages',
-        data: {
-            projectType: $scope.projectType
-        }
-    })
-    .then(function (response) {
-        console.log(response);
-    },
-    function (response) {
-
+    $scope.$watch(Account.getUser, function (newValue, oldValue) {
+        $scope.user = angular.copy(newValue);
     });
 
+    $scope.$watch(packagesService.getNewPackages, function (newValue, oldValue) {
+        if (newValue.status == 1) {
+            $scope.projectType = newValue.type;
+            $scope.getProjects();
+
+            var _newPackages = {status:0,type:''}
+            packagesService.setNewPackages(_newPackages);
+        }
+    });
+
+    $scope.projectType  = 'a';
+    $scope.items        = [];
+
+    $scope.getProjects = function () {
+        $http({
+            method: 'POST',
+            url: '/api/wiki/models/packages',
+            data: {
+                projectType: $scope.projectType
+            }
+        })
+        .then(function (response) {
+            $scope.items = response.data
+        },
+        function (response) {});
+    }
+
+    $scope.getProjects();
+
+    $scope.ShowCreateProjectDialog = function () {
+        $uibModal.open({
+            templateUrl: WIKI_WEBROOT + "partials/packages_project_create.html",
+            controller: "packagesProjectsCreateController",
+        }).result.then(function (params) {
+            alert(params);
+        }, function (text, error) {
+
+        });
+    };
+
+    $scope.ShowModifyProjectDialog = function () {
+        $uibModal.open({
+            templateUrl: WIKI_WEBROOT + "partials/packages_project_modify.html",
+            controller: "packagesProjectsModifyController",
+        }).result.then(function (params) {
+            
+        }, function (text, error) {
+
+        });
+    }
+
+    $scope.DeleteProject = function () {
+        if (confirm("Are you sure delete this project?")) {
+            alert('OK!!!');
+        }
+    }
+
+    $scope.setTabs = function (params) {
+        $scope.projectType = params;
+        $scope.getProjects();
+    }
+
+    //$scope.$on('updateItems', function (event,msg) {
+    //    console.log(msg);
+    //    //$scope.getProjects();
+    //});
+})
+.controller('packagesProjectsCreateController', function (Account, $scope, $http, $uibModalInstance, packagesService) {
+    $scope.projectName = 'My fitst Project';
+    $scope.projectDesc = 'My fitst Project DESC';
+    $scope.projectGitURL = 'https://github.com/onedou/DOC';
+    $scope.projectType = 'a';
+    $scope.projectTypeName = '';
 
     $scope.$watch(Account.getUser, function (newValue, oldValue) {
         $scope.user = angular.copy(newValue);
@@ -24,29 +102,10 @@ angular.module('MyApp')
     $scope.$watch('projectType', function (newValue, oldValue) {
         if (newValue == 'a') {
             $scope.projectTypeName = 'npl package';
-        } else if(newValue == 'b') {
+        } else if (newValue == 'b') {
             $scope.projectTypeName = 'paracraft mod';
         }
     });
-
-    $scope.tabsActive = 1;
-    
-    $scope.projectName     = 'My fitst Project';
-    $scope.projectDesc     = 'My fitst Project DESC';
-    $scope.projectGitURL   = 'https://github.com/onedou/DOC';
-    $scope.projectType     = 'a';
-    $scope.projectTypeName = '';
-
-    $scope.ShowCreateProjectDialog = function () {
-        $uibModal.open({
-            templateUrl: WIKI_WEBROOT + "partials/packages_project_create.html",
-            controller: "packagesProjectsController",
-        }).result.then(function (proj) {
-            
-        }, function (text, error) {
-
-        });
-    };
 
     $scope.confirm = function () {
         var gitRaw       = "https://raw.githubusercontent.com";
@@ -90,44 +149,22 @@ angular.module('MyApp')
         });
 
         $http.post('/api/wiki/models/packages/createPackage', {
-            projectName   : $scope.projectName,
-            projectDesc   : $scope.projectDesc,
-            projectGitURL : $scope.projectGitURL,
-            projectType   : $scope.projectType,
-            UserId        : $scope.user._id
+            projectName: $scope.projectName,
+            projectDesc: $scope.projectDesc,
+            projectGitURL: $scope.projectGitURL,
+            projectType: $scope.projectType,
+            UserId: $scope.user._id
         })
         .then(function (response) {
-            console.log(response);
+            if (response.data.result == 1) {
+                //$scope.$emit('updateItems', 'OKOKOK!');
+                var _newPackages = { status: 1, type: $scope.projectType }
+                packagesService.setNewPackages(_newPackages);
+                $uibModalInstance.close(response.data.msg);
+            }
         }, function (error) {});
     }
+})
+.controller('packagesProjectsModifyController', function (Account, $scope, $http, $uibModalInstance, packagesService) {
 
-    $scope.ShowModifyProjectDialog = function () {
-        $uibModal.open({
-            templateUrl: WIKI_WEBROOT + "partials/packages_project_modify.html",
-            controller: "packagesProjectsController",
-        }).result.then(function (proj) {
-            $http.put("/api/wiki/models/project/new", {
-                name: proj.name,
-                fork: proj.fork,
-                color: proj.color,
-            }).then(function (response) {
-                $scope.lastCreatedProjName = proj.name;
-                $scope.getProjects();
-            }).catch(function (response) {
-                console.log("error:" + response.data.message);
-            });
-        }, function (text, error) {
-
-        });
-    }
-
-    $scope.DeleteProject = function () {
-        if (confirm("Are you sure delete this project?")) {
-            alert('OK!!!');
-        }
-    }
-
-    $scope.setTabs = function (params) {
-        $scope.tabsActive = params;
-    }
 })
