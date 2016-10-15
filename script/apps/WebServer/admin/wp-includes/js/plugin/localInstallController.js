431289f33f260ebe98e4cb680a74fb6c120def20
onedou <onedou@126.com>
2016-10-15 23:50:16 +0800

    update local plugin install api (#12)
    * Initial commit
    
    * #modify
    home.page
    packages_npl.page
    
    #add
    icommon.woff2
    packages.css
    packages.less
    
    #delete
    packages.page
    
    Signed-off-by: onedou <onedou@126.com>
    
    * work for packages_npl.page
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update packages_npl and packages_npl_install pages
    
    Signed-off-by: onedou <onedou@126.com>
    
    * install packages page
    
    Signed-off-by: onedou <onedou@126.com>
    
    * project pages
    
    Signed-off-by: onedou <onedou@126.com>
    
    * packages projects create
    and other document modify
    
    Signed-off-by: onedou <onedou@126.com>
    
    * package project model
    
    Signed-off-by: onedou <onedou@126.com>
    
    * projects mod update
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update packages.page
    
    Signed-off-by: onedou <onedou@126.com>
    
    * 需求文档
    
    * update .gitignore
    
    * packages update
    
    * packages update
    
    Signed-off-by: onedou <onedou@126.com>
    
    * packages updates
    
    Signed-off-by: onedou <onedou@126.com>
    
    * remove tmp files
    
    Signed-off-by: onedou <onedou@126.com>
    
    * fixed packages stats
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update localInstall.page
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update localInstall.page
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update local instal
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update local install
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update local package install
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update localInstall.page
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update localInstall.page
    
    Signed-off-by: onedou <onedou@126.com>
    
    * localinstall update
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update localinstall template
    
    Signed-off-by: onedou <onedou@126.com>
    
    * temporary operation
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update localInstall.page
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update localInstall
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update localInstall
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update local install
    
    Signed-off-by: onedou <onedou@126.com>
    
    * local install update
    
    * local install update
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update local install
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update local install
    
    Signed-off-by: onedou <onedou@126.com>
    
    * update local install
    
    Signed-off-by: onedou <onedou@126.com>

---
+++ script/apps/WebServer/admin/wp-includes/js/plugin/localInstallController.js
@@ -0,0 +1,127 @@
+﻿angular.module('plugin', ['ui.bootstrap'])
+.controller('localInstallController', function ($scope, $http, $location, $interval,$timeout) {
+    var params = $location.search();
+
+    $scope.projectName = params.projectName;
+    $scope.version = params.version;
+    $scope.author = params.displayName;
+    $scope.giturl = params.giturl;
+    $scope.packagesId = params.packagesId;
+
+    $scope.seconds = 5;
+    $scope.currentProjectName = 'Not yet!';
+    $scope.waitPackagesIsEmpty = true;
+    $scope.waitPackages = [];
+
+    $interval(function () {
+        $scope.seconds--;
+    },1000,5);
+
+    $timeout(function () {
+        $scope.install();
+        $(".start").text("Starting......");
+    }, 5000);
+
+    $scope.install = function () {
+        $http({
+            method: 'POST',
+            url: '/ajax/localInstall?action=downloadQueue',
+            data: {
+                url: $scope.giturl,
+                projectName: $scope.projectName,
+                packagesId: $scope.packagesId
+            }
+        })
+        .then(function (response) {
+            if (response.data.lock == 1) {
+                $timeout(function () {
+                    $scope.install();
+                }, 1000);
+                return;
+            }
+
+            $scope.currentProjectName = response.data.currentProjectName;
+
+            for (var i in response.data.waitPackages) {
+                $scope.waitPackagesIsEmpty = false;
+                break;
+            }
+            
+            if (!$scope.waitPackagesIsEmpty) {
+                $scope.waitPackages = response.data.waitPackages;
+            }
+
+            if (response.data.isYourTurn == 1) {
+                if (response.data.status == 1) {
+                    $scope.getCurrentDownload();
+                } else if (response.data.status == 0) {
+                    $(".start").text("packages is not update");
+                    $(".button span").css("display", "block");
+                } else if (response.data.status == -1) {
+                    $(".start").text("service is not available now,please try again later");
+                    $(".button span").css("display", "block");
+                }
+
+            } else if (response.data.currentPackagesId == $scope.packagesId) {
+                $scope.getCurrentDownload();
+                //alert('continue');
+            } else if (response.data.isYourTurn == 0) {
+                console.log(response.data);
+
+                $timeout(function () {
+                    $scope.install();
+                }, 1000);
+            } else {
+                return alert("error!");
+            }
+
+        }, function (response) { });
+    }
+
+    $scope.getCurrentDownload = function () {
+        $http({
+            method: "GET",
+            url: "/ajax/localInstall?action=GetCurrentDownload",
+            data: {
+
+            }
+        })
+        .then(function (response) {
+            if (response.data.status == -1 || response.data.status == 0) {
+
+                var currentFileSize = response.data.currentFileSize;
+                var totalFileSize   = response.data.totalFileSize;
+
+                var percent = (currentFileSize / totalFileSize) * 100;
+
+                //console.log(currentFileSize, totalFileSize, percent)
+
+                if (isNaN(percent) || percent == Infinity) {
+                    percent = 0;
+                } else {
+                    percent = parseInt(percent);
+                }
+
+                //console.log(currentFileSize, totalFileSize, percent);
+
+                $(".start").text(percent + "%      " + parseInt(currentFileSize / 1024) + "/" + parseInt(totalFileSize / 1024) + " KB");
+                $(".process").css({ "display": "block", "width": percent + "%" });
+
+                $timeout(function () {
+                    $scope.getCurrentDownload();
+                }, 1000);
+                
+            } else if (response.data.status == 1) {
+
+                $(".start").text("Download complete!").css("background-color", "#00ffbd");
+                $(".process").css({ "opacity": "0", "width": "100%" });
+                $(".button span").css("display", "block");
+
+            } else {
+                return alert("error status!");
+            }
+
+        }, function (response) { });
+
+    }
+})
