2553a32fb1eb4c54c89d2b78286ae0bc420ea082
xiaoyao <765485868@qq.com>
2016-10-19 21:33:05 +0800

    增加页面导航模板相关功能

--- www/wiki/data/website_template_style.sh
+++ www/wiki/data/website_template_style.sh
@@ -7,4 +7,5 @@ curl -H "Content-Type: application/json" -X PUT  --data '{"name":"空样式", "t
 curl -H "Content-Type: application/json" -X PUT  --data '{"name":"空模板灰色", "templateId":1, "filename":"blank_gray_style_tpl.html", "content":"<div style=\"background-color: #8cbbad\">__PageContent__</div>"}'  "${serverAddr}/api/wiki/models/website_template_style/new"
 curl -H "Content-Type: application/json" -X PUT  --data '{"name":"空模板灰色", "templateId":2, "filename":"blank_gray_style_tpl.html", "content":"<div style=\"background-color: #8cbbad\">__PageContent__</div>"}'  "${serverAddr}/api/wiki/models/website_template_style/new"
 
+curl -H "Content-Type: application/json" -X PUT  --data '{"name":"导航模板样式", "templateId":1, "filename":"nav_tpl.html","content":"<div>__PageContent__</div>"}'  "${serverAddr}/api/wiki/models/website_template_style/new"
 #curl -H "Content-Type: application/json" -X POST --data '{}' "localhost:8099/api/wiki/models/website_template_style"
