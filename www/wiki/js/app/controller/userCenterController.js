9448df70d81cd5dba58f136d50fde4794b3aa304
xiaoyao <765485868@qq.com>
2017-02-16 17:35:34 +0800

    xxx

--- www/wiki/js/app/controller/userCenterController.js
+++ www/wiki/js/app/controller/userCenterController.js
@@ -104,7 +104,7 @@ define(['app', 'helper/util', 'helper/storage', 'text!html/userCenter.html'], fu
 
         // data source
         $scope.clickMyDataSource = function () {
-            $scope.githubDS = ($scope.user.github && $scope.user.github) > 0 ? true : false;
+            $scope.githubDS = ($scope.user.githubId && $scope.user.githubId) > 0 ? true : false;
         }
         $scope.githubDSChange = function () {
             if ($scope.githubDS) {
