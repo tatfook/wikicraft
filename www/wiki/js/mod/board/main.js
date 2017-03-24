4f89ad325efc0762262c96583efbbd561b115f03
CYF <me@caoyongfeng.com>
2017-03-24 11:31:01 +0800

    modify mod (draw board)

--- www/wiki/js/mod/board/main.js
+++ www/wiki/js/mod/board/main.js
@@ -195,61 +195,24 @@
                             //this.canvas.renderAll();
                         });
                     },
-                    //_render: function (ctx) {
-                    //    this.callSuper('_render', ctx);
-
-                    //    //this.point1.callSuper('_render', ctx);
-                    //    //this.point2.callSuper('_render', ctx);
-                    //    //if (!window._isAppendedChilds) {
-                    //    //    console.log('AAAAAAAAAAAA');
-                    //    //    console.log(this);
-                    //    //    this.canvas.add(this.point1);
-                    //    //    window._isAppendedChilds = true;
-                    //    //    //
-                    //    //    //this.canvas.add(this.point2);
-                    //    //    //this._isAppendedChilds = true;
-                    //    //}
-                        
-                    //    //ctx.save();
-                    //    //ctx.beginPath();
-                    //    //ctx.strokeStyle = 'blue';
-                    //    //ctx.lineWidth = 2;
-                    //    //ctx.moveTo(0, 3);
-                    //    //ctx.lineTo(this.width, 3);
-                    //    //ctx.stroke();
-                    //    //ctx.restore();
-
-                    //    //ctx.save();
-                    //    //ctx.beginPath();
-                    //    //ctx.strokeStyle = ctx.fillStyle = 'red';
-                    //    //ctx.lineWidth = 5;
-                    //    //ctx.arc(this.x1 + 5, this.y1 + 5, 10, 0, 2 * Math.PI, true);
-                    //    //ctx.stroke();
-                    //},
-                    //render: function (ctx) {
-                    //    this.callSuper('render', ctx);
-                    //    console.log('FFFFFFFFFf');
-                    //    //console.log(this.canvas.add);
-                    //    if (!this.point1._isin) {
-                    //        this.point1._isin = true;
-                    //        this.canvas.add(this.point1);
-                    //    }
-
-                    //    //this.point1.set({
-                    //    //    left: this.left - this.point1.radius,
-                    //    //    top: this.top - this.point1.radius
-                    //    //});
-                    //    //this.point2.set({
-                    //    //    left: this.x2 - this.point2.radius,
-                    //    //    top: this.y2 - this.point2.radius
-                    //    //});
-                    //    //this.point1.render(ctx);
-                    //}
+                    toObject: function () {
+                        return fabric.util.object.extend(this.callSuper('toObject'), {
+                            //customAttribute: this.get('customAttribute')
+                        });
+                    },
+                    _render: function (ctx) {
+                        this.callSuper('_render', ctx);
+                    }
                 });
+
+                fabric.Link.fromObject = function (object, callback, forceAsync) {
+                    //return fabric.Object._fromObject('Link', object, callback, forceAsync, 'line');
+                    return fabric.Line.fromObject.apply(this, arguments);
+                };
 
                 // 连接线端点
                 fabric.LinkPort = fabric.util.createClass(fabric.Circle, {
-                    type: 'linkport',
+                    type: 'link-port',
                     initialize: function (options, link, portIndex) {
                         options || (options = {});
                         options.lockScalingX = true;
@@ -482,8 +445,21 @@
                         };
 
 
+                    },
+                    toObject: function () {
+                        return fabric.util.object.extend(this.callSuper('toObject'), {
+                            //customAttribute: this.get('customAttribute')
+                        });
+                    },
+                    _render: function (ctx) {
+                        this.callSuper('_render', ctx);
                     }
                 });
+
+                fabric.LinkPort.fromObject = function (object, callback, forceAsync) {
+                    //return fabric.Object._fromObject('Link', object, callback, forceAsync, 'line');
+                    return fabric.Circle.fromObject.apply(this, arguments);
+                };
 
                 var init_show = function (width, height, data) {
                     //cvs_show.dataset.width = width;
@@ -647,46 +623,47 @@
                     $uibModal.open({
                         template: `
                             <div class ="modal-header" style="display:flex;display:-webkit-flex;align-items:center;-webkit-align-items:center">
-                                <h3 class ="modal-title" style="min-width:100px;flex:1;">画板</h3>
-                                <div>
-                                    <div ng-repeat="item in editItems" class="pull-left" style="margin-right:25px;">
-                                        <div ng-switch="item">
-                                            <div ng-switch-when="fill">
+                                <h3 class ="modal-title" style="min-width:100px">画板</h3>
+                                <div style="flex-basis:100%;-webkit-flex-basis:100%;-moz-flex-basis:100%;-ms-flex-basis:100%;-o-flex-basis:100%;text-align:right">
+                                    <span ng-repeat="item in editItems">
+                                        <span ng-switch="item">
+                                            <span ng-switch-when="fill">
                                                 填充色：<color-selector onchange="fillColorChanged" value="selectedShape.type == 'group' ? selectedShape._objects[0].fill : selectedShape.fill"></color-selector>
-                                            </div>
-                                            <div ng-switch-when="stroke">
+                                            </span>
+                                            <span ng-switch-when="stroke">
                                                 边框色：<color-selector onchange="strokeColorChanged" value="selectedShape.type == 'group' ? selectedShape._objects[0].stroke : selectedShape.stroke"></color-selector>
-                                            </div>
-                                            <div ng-switch-when="color">
+                                            </span>
+                                            <span ng-switch-when="color">
                                                 字体色：<color-selector onchange="fontColorChanged" value="selectedShape.type == 'group' ? selectedShape._objects[1].fill : selectedShape.fill"></color-selector>
-                                            </div>
-                                            <div ng-switch-when="fontSize">
+                                            </span>
+                                            <span ng-switch-when="fontSize">
                                                 字体大小：<number-selector onchange="fontSizeChanged" min="'13'" max="'29'" step="'2'" unit="'px'" value="selectedShape.type == 'group' ? selectedShape._objects[1].fontSize : selectedShape.fontSize"></number-selector>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div ng-show="selectedShape" class="pull-left">
-                                        <span class="glyphicon glyphicon-trash" ng-click="removeShape()" style="font-size:20px;line-height:24px;"></span>
-                                    </div>
+                                            </span>
+                                        </span>
+                                    </span>
+                                    <span ng-show="selectedShape">
+                                        |
+                                        <button type="button" class="btn btn-danger" ng-click="removeShape()">删除</button>
+                                    </span>
                                 </div>
                             </div>
-                            <div class="modal-body" style="display:flex;display:-webkit-flex;padding:0;">
-                                <div style="width:200px;min-width:200px;background-color:#FFF;margin-top:20px;padding:0 3px 0 5px;">
+                            <div class="modal-body" style="display:flex;display:-webkit-flex">
+                                <div style="width:200px;min-width:200px;background-color:#444;">
                                     <div style="display:flex;display:-webkit-flex;flex-wrap:wrap;-webkit-flex-wrap:wrap;-moz-flex-wrap:wrap;-ms-flex-wrap:-o-wrap">
-                                        <div ng-repeat="item in items" ng-click="itemClick()" style="flex-basis:50%;-webkit-flex-basis:50%;-moz-flex-basis:50%;-ms-flex-basis:50%;-o-flex-basis:50%;text-align:center;margin-bottom:20px;">
-                                            <button type="button" title="{{item.name}}" class ="btn btn-default btn-lg" style="width:90px;height:50px;padding:0px;border-radius:0px;background-color:#DCDCDC;">
+                                        <div ng-repeat="item in items" ng-click="itemClick()" style="flex-basis:50%;-webkit-flex-basis:50%;-moz-flex-basis:50%;-ms-flex-basis:50%;-o-flex-basis:50%;text-align:center">
+                                            <button type="button" title="{{item.name}}" class ="btn btn-default btn-lg active" style="width:90px;height:50px;padding:0px;border-radius:0px">
                                                 <span style="display:inline-block;width:42px;height:30px;background-image:url('/wiki/js/mod/board/shapes.png');" ng-style="{backgroundPosition:-$index*42+'px 0px'}"></span>
                                             </button>
                                             <br/>{{item.name}}
                                         </div>
                                     </div>
                                 </div>
-                                <div style="flex-basis:100%;-webkit-flex-basis:100%;-moz-flex-basis:100%;-ms-flex-basis:100%;-o-flex-basis:100%;background-color:#DCDCDC;overflow:auto;">
+                                <div style="flex-basis:100%;-webkit-flex-basis:100%;-moz-flex-basis:100%;-ms-flex-basis:100%;-o-flex-basis:100%;background-color:#f8f8f8">
                                     <canvas scope-element="cvs"></canvas>
                                 </div>
                             </div>
                             <div class ="modal-footer">
-                                <button class ="btn btn-default" type="button" data-dismiss="modal" ng-click="cancel()">取消</button>
+                                <button class ="btn btn-warning" type="button" data-dismiss="modal" ng-click="cancel()">取消</button>
                                 <button class="btn btn-primary" type="button" ng-click="save()">保存</button>
                             </div>
                         `,
@@ -1000,7 +977,7 @@
         render: function (wikiBlock) {
             registerController(wikiBlock);
             return `
-                <div ng-controller="boardController" ng-click="onclick()" style="min-height:100px;border: 1px solid #DCDCDC;">
+                <div ng-controller="boardController" ng-click="onclick()" style="min-height:100px">
                     <canvas scope-element="cvs_show"></canvas>
                 </div>
                 `;
