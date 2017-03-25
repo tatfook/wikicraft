b957e4163d81d39f91ead1ff6eea9f6db2c612f9
zhangleio <zhangleio@outlook.com>
2017-03-25 22:06:17 +0800

    Chanaged readme.md

--- www/wiki/js/mod/model/main.js
+++ www/wiki/js/mod/model/main.js
@@ -1,7 +1,6 @@
 define(function () {
     function registerController(wikiBlock) {
         app.registerController("modelController", function ($scope, $uibModal, $timeout) {
-            console.log("========================modelController", 0xffffff);
 
             function convert16_32(rgb) {
                 var a = (rgb & 0xF000) << 16;
@@ -13,30 +12,58 @@ define(function () {
                 return v;
 
             }
+            function parseText(txt) {
+                var data = Lua.eval(txt)
+                if(data && data.length > 0){
+                    return data[0];
+                }
+            }
+            function parseBlockModel() {
+                var blockmodel = $('pe\\:model', wikiBlock.modParams);
+                if (blockmodel && blockmodel[0])
+                {
+                    var url = blockmodel[0].innerText;
+                    console.log("=========url", url);
+                    $.get(url)
+                      .done(function (d) {
+                          console.log(d);
+                      })
+                    return true;
+                }
+                return false;
 
+            }
+            function parseBlockNode() {
+                var block_node = $('pe\\:blocks', wikiBlock.modParams);
+                if (block_node && block_node[0]) {
+                    var txt = block_node[0].innerText;
+
+                    var blocks_list = parseText(txt);
+                    create_meshes(blocks_list);
+                }
+            }
+            function create_meshes(blocks_list) {
+                if (!blocks_list) {
+                    return;
+                }
+                for (var i = 0; i < blocks_list.length; i++) {
+                    var block = blocks_list[i];
+                    if (block.length > 4) {
+                        var x = block[0];
+                        var y = block[1];
+                        var z = block[2];
+                        var block_id = block[3];
+                        var color = block[4];
+                        $scope.view.createMesh(x, y, z, block_id, convert16_32(color));
+                    }
+                }
+            }
             require(["THREE", "THREE_OrbitControls", "THREE_TransformControls", "THREE_ThreeJsView"], function () {
                 var container = $scope.scopeElements.container;
-                var view = new THREE.ThreeJsView(container);
+                $scope.view = new THREE.ThreeJsView(container);
 
-                var xmlNodes = $('pe\\:blocks', wikiBlock.modParams);
-                if (xmlNodes && xmlNodes[0]) {
-                    var txt = xmlNodes[0].innerText;
-                    if (txt) {
-                        var data = Lua.eval(txt)
-                        var blocks_list = data[0]
-                        for (var i = 0; i < blocks_list.length; i++) {
-                            var block = blocks_list[i];
-                            if (block.length > 4) {
-                                var x = block[0];
-                                var y = block[1];
-                                var z = block[2];
-                                var block_id = block[3];
-                                var color = block[4];
-                                view.createMesh(x, y, z, block_id, convert16_32(color));
-                            }
-                        }
-                    }
-                    
+                if (!parseBlockModel()) {
+                    parseBlockNode();
                 }
                 
             });
