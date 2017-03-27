99eb99537e5ec6c66ca78c782ce646f30eff628d
ZhangKaitlyn <1873070425@qq.com>
2017-03-27 14:05:45 +0800

    导航条小屏调整；3D世界model

---
+++ www/wiki/js/mod/wiki/js/world3D.js
@@ -0,0 +1,27 @@
+/**
+ * Created by wuxiangan on 2017/3/21.
+ */
+
+define([
+    'app',
+    'helper/util',
+    'text!wikimod/wiki/html/world3D.html'
+], function (app, util, htmlContent) {
+    function registerController(wikiblock) {
+        app.registerController('world3DController',['$scope', function ($scope) {
+            $scope.imgsPath = config.wikiModPath + 'wiki/assets/imgs/';
+            $scope.modParams = angular.copy(wikiblock.modParams || {});
+        }]);
+    }
+    return {
+        render: function(wikiblock){
+            registerController(wikiblock);
+            return htmlContent;
+        }
+    };
+})
+
+/*
+```@wiki/js/world3D
+```
+*/
