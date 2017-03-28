d6a9be83c799144f5ebd801bd0c41b9e26e7305d
xiaoyao <765485868@qq.com>
2017-03-28 16:20:50 +0800

    xxx

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 workDir=`pwd`
 #npl -d bootstrapper="script/apps/WebServer/WebServer.lua" ip="10.0.2.15" port="8099" root="www/" dev="${workDir}"
+
+echo $1
+if [ $1 = “build” ]; then
+npl -d bootstrapper="script/apps/WebServer/WebServer.lua"  root="www_build/" port="8900" dev="${workDir}"
+else
 npl -d bootstrapper="script/apps/WebServer/WebServer.lua"  root="www/" port="8099" dev="${workDir}"
-#npl -d bootstrapper="script/apps/WebServer/WebServer.lua"  root="www_build/" port="8099" dev="${workDir}"
+fi
 
