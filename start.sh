4bbcd1db79f837ec3540991005010c3f14b6d596
xiaoyao <765485868@qq.com>
2017-03-28 20:25:36 +0800

    xx

--- start.sh
+++ start.sh
@@ -7,13 +7,13 @@ if [ "$1" = "build" ]; then
 	if [ ! -z $pid ]; then
 		kill -9 $((pid))
 	fi
-	npl -d bootstrapper="script/apps/WebServer/WebServer.lua"  root="www_build/" port="8900" dev="${workDir}"
+	npl -d bootstrapper="script/apps/WebServer/WebServer.lua"  root="www_build/" port="8900" logfile="build_log.log" dev="${workDir}"
 else
 	echo "start server..."
 	pid=`ps uax | grep "npl.*port=8099.*" | grep -v grep | awk '{print $2}'`
 	if [ ! -z $pid ]; then
 		kill -9 $((pid))
 	fi
-	npl -d bootstrapper="script/apps/WebServer/WebServer.lua"  root="www/" port="8099" dev="${workDir}"
+	npl -d bootstrapper="script/apps/WebServer/WebServer.lua"  root="www/" port="8099" logfile="dev_log.log" dev="${workDir}"
 fi
 
