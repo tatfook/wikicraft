dd19201d1e738437bc4f685d556846452622cc73
xiaoyao <765485868@qq.com>
2017-03-29 21:55:26 +0800

    main.js加版本后缀

--- www/wiki/js/app/config.js
+++ www/wiki/js/app/config.js
@@ -54,7 +54,7 @@
         pageUrlPrefix:'/wiki/html/',
 
         // api接口路径
-        apiUrlPrefix:localEnv ? 'http://'+window.location.host+'/api/wiki/models/' : ('http://' + officialDomain + '/api/wiki/models/'),
+        apiUrlPrefix:'http://' + (localEnv ? (localVMEnv ? window.location.host : "localhost:8900") : officialDomain ) + '/api/wiki/models/',
         //modulePageUrlPrefix:'/wiki/module',
         //moduleApiUrlPrefix:'http://localhost:8099/api/module/',  // + moduleName + "/models/" + modelName + '[apiName]'
         // --------------------------------------路径配置 END----------------------------------------
