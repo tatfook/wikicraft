ef61f195ea64391d86083a213ea431ac2e0b7a70
xiaoyao <765485868@qq.com>
2017-04-11 22:49:33 +0800

    xxx

--- www/wiki/js/mod/wiki/js/richText.js
+++ www/wiki/js/mod/wiki/js/richText.js
@@ -8,10 +8,12 @@ define([
 ], function (app, wangEditor) {
     function registerController(wikiblock) {
         app.registerController('richTextController',['$rootScope','$scope', function ($rootScope, $scope) {
+            var editor = undefined;
+            var isDestroyEditor = false;
             $scope.imgsPath = config.wikiModPath + 'wiki/assets/imgs/';
             //console.log(wikiblock.modParams);
             $scope.modParams = angular.copy(wikiblock.modParams || {});
-
+            $scope.isEditor = false;
             //console.log("init pageListController");
 
             function init() {
@@ -19,19 +21,33 @@ define([
             }
             
             $scope.viewEditor = function () {
-                var editor = $(event.target).children()[0];
+                //console.log(editor);
+                $scope.isEditor = true;
+                if (editor) {
+                    if (isDestroyEditor) {
+                        editor.undestroy();
+                        isDestroyEditor = false;
+                    }
+                    return ;
+                }
+                editor = $(event.target).children()[0];
                 editor = new wangEditor(editor);
-                console.log(editor.config);
+                console.log(editor.config, wangEditor);
                 editor.create();
+                editor.$txt.on("blur", function () {
+                    $scope.isEditor = false;
+                    editor.destroy();
+                    isDestroyEditor = true;
+                });
             }
-            
+
             $scope.$watch("$viewContentLoaded", init);
         }]);
     }
     return {
         render: function(wikiblock){
             registerController(wikiblock);
-            return '<div ng-controller="richTextController" ng-click="viewEditor()" style="height: 30px; border: 1px solid"><div></div><div ng-bind-html="htmlContent"></div></div>';
+            return '<div ng-controller="richTextController" ng-click="viewEditor()" style="height: 130px; border: 1px solid"><div></div></div>';
         }
     };
 });
