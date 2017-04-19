846a33c7981641809edc45cd13359438243ec2e0
xiaoyao <765485868@qq.com>
2017-04-19 18:27:48 +0800

    xxx

--- www/wiki/js/app.js
+++ www/wiki/js/app.js
@@ -31,9 +31,20 @@ define([
         $authProvider.github({
             url: "/api/wiki/auth/github",
             clientId: '2219fe9cb6d105dd30fb',
-            redirectUri: window.location.origin + '/wiki/login',
+            redirectUri: (config.isLocal() ? 'http://dev.keepwork.com/' : window.location.origin) + '/wiki/login',
             // scope: ["public_repo", "delete_repo"],
             scope: ["public_repo"],
+        });
+
+        $authProvider.oauth2({
+            name: 'qq',
+            url: '/api/wiki/auth/qq',
+            //responseType:"token",
+            clientId: '100302176',
+            redirectUri: 'http://dev.keepwork.com/api/wiki/auth/qq',//window.location.origin,
+            authorizationEndpoint: 'https://graph.qq.com/oauth2.0/authorize',
+            oauthType: '2.0',
+            scope:'get_user_info',
         });
     }]);
 
