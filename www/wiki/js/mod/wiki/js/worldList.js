708f2f3c1c1e8d2b6ebd067df781c10f0d97ec37
onedou <onedou@126.com>
2017-04-28 17:35:21 +0800

    update worldList

--- www/wiki/js/mod/wiki/js/worldList.js
+++ www/wiki/js/mod/wiki/js/worldList.js
@@ -8,6 +8,89 @@ define([
     'text!wikimod/wiki/html/worldList.html'
 ], function (app, util, htmlContent) {
     function registerController(wikiblock) {
+        function Dig2Dec(s) {
+            var retV = 0;
+            if (s.length == 4) {
+                for (var i = 0; i < 4; i++) {
+                    retV += eval(s.charAt(i)) * Math.pow(2, 3 - i);
+                }
+                return retV;
+            }
+            return -1;
+        }
+
+        function Dec2Dig(n1) {
+            var s = "";
+            var n2 = 0;
+            for (var i = 0; i < 4; i++) {
+                n2 = Math.pow(2, 3 - i);
+                if (n1 >= n2) {
+                    s += '1';
+                    n1 = n1 - n2;
+                }
+                else
+                    s += '0';
+            }
+            return s;
+        }
+
+        function Str2Hex(s) {
+            var c = "";
+            var n;
+            var ss = "0123456789ABCDEF";
+            var digS = "";
+            for (var i = 0; i < s.length; i++) {
+                c = s.charAt(i);
+                n = ss.indexOf(c);
+                digS += Dec2Dig(eval(n));
+            }
+            return digS;
+        }
+
+        function Hex2Utf8(s) {
+            var retS = "";
+            var tempS = "";
+            var ss = "";
+            if (s.length == 16) {
+                tempS = "1110" + s.substring(0, 4);
+                tempS += "10" + s.substring(4, 10);
+                tempS += "10" + s.substring(10, 16);
+                var sss = "0123456789ABCDEF";
+                for (var i = 0; i < 3; i++) {
+                    retS += "%";
+                    ss = tempS.substring(i * 8, (eval(i) + 1) * 8);
+                    retS += sss.charAt(Dig2Dec(ss.substring(0, 4)));
+                    retS += sss.charAt(Dig2Dec(ss.substring(4, 8)));
+                }
+                return retS;
+            }
+            return "";
+        }
+
+        function gb2utf8(s1) {
+            var s = escape(s1);
+            var sa = s.split("%");
+            var retV = "";
+            if (sa[0] != "") {
+                retV = sa[0];
+            }
+            for (var i = 1; i < sa.length; i++) {
+                if (sa[i].substring(0, 1) == "u") {
+                    retV += Hex2Utf8(Str2Hex(sa[i].substring(1, 5)));
+                    if (sa[i].length) {
+                        retV += sa[i].substring(5);
+                    }
+                }
+                else {
+                    retV += unescape("%" + sa[i]);
+                    if (sa[i].length) {
+                        retV += sa[i].substring(5);
+                    }
+                }
+            }
+            return retV;
+        }
+
         app.directive('preview', [function () {
             return {
                 restrict: "E",
@@ -43,6 +126,8 @@ define([
                 } else {
                     this.filesTotals = "0KB";
                 }
+
+                this.readme = decodeURI(gb2utf8(this.readme));
             });
 
             $scope.opusTotals = $scope.modParams.length;
