7144fc7c41b1e5f3cfae18798ed671d26ee81c07
xiaoyao <765485868@qq.com>
2017-05-15 11:29:12 +0800

    更新启动和打包脚本

---
+++ r_package.js
@@ -0,0 +1,123 @@
+/**
+ * Created by wuxiangan on 2017/1/5.
+ */
+
+({
+    appDir:"www",
+    dir:'www_build',
+    baseUrl:'wiki/js',
+    //name:'main',
+    optimizeCss: 'standard',
+    removeCombined:true,
+    optimizeAllPluginResources: true,  // text 插件配置
+    modules:[
+        {
+            name:'main'
+        },
+    ],
+    //out:'build/main.js',
+    paths:{
+        'jquery': 'lib/jquery/jquery.min',
+        'jquery-cookie': 'lib/jquery-cookie/jquery.cookie',
+        'angular': 'lib/angular/angular.min',
+        'angular-ui-bootstrap': 'lib/angular-ui-bootstrap/ui-bootstrap-tpls',
+        'angular-toggle-switch': 'lib/angular-toggle-switch/angular-toggle-switch.min',
+        'angular-ui-select': 'lib/angular-ui-select/select.min',
+        'angular-sanitize': 'lib/angular-sanitize/angular-sanitize.min',
+        'bootstrap': "lib/bootstrap/js/bootstrap.min",
+        'satellizer': 'lib/satellizer/satellizer.min',
+        'bootstrap-treeview': 'lib/bootstrap-treeview/bootstrap-treeview.min',
+        'github-api': 'lib/github-api/GitHub.bundle.min',
+        'cropper': 'lib/cropper/cropper.min',
+        'markdown-it':'lib/markdown-it/markdown-it.min',
+        'highlight': 'lib/highlight/highlight.pack', 
+        'js-base64': 'lib/js-base64/base64.min',
+        'text': 'lib/requirejs/text',
+        'domReady': 'lib/requirejs/domReady',
+		'jquery-sharejs': 'lib/sharejs/js/jquery.share.min',// 社交分享
+		'contribution-calendar': 'lib/contribution-calendar/js/contribution-calendar.min',//类github活动记录
+		'to-markdown': 'lib/to-markdown/to-markdown',
+		'wangEditor': 'lib/wangEditor/js/wangEditor',
+
+        // 自定义模块
+        'app': 'app',
+        'router': 'router',
+        'preload': 'app/preload',
+
+        // dir map
+        'controller': 'app/controller',
+        'directive': 'app/directive',
+        'factory': 'app/factory',
+        'helper': 'app/helper',
+        // html dir
+        'html': '../html'
+    },
+    shim: {
+        'angular': {
+            deps:['jquery'],
+            exports: 'angular',
+        },
+        'angular-ui-router':{
+            deps:['angular'],
+        },
+        'angular-ui-bootstrap':{
+            deps:['angular'],
+        },
+        'angular-ui-select':{
+            deps:['angular'],
+        },
+        'angular-sanitize':{
+            deps:['angular'],
+        },
+        'satellizer':{
+            deps:['angular'],
+        },
+        'bootstrap':{
+            deps:['jquery'],
+        },
+        'cropper':{
+            deps:['jquery'],
+        },
+        'bootstrap-treeview': {
+            deps:['bootstrap', 'jquery'],
+        },
+        'highlight':{
+            exports: 'hljs',
+        },
+		'jquery-cookie': {
+			deps:['jquery'],
+		},
+		'jquery-sharejs':{
+			deps:['jquery'],
+		},
+		'wangEditor': {
+			deps:['jquery'],
+		}
+    },
+    packages: [
+        {
+            name: "codemirror",
+            location: "lib/codemirror",
+            main: "lib/codemirror"
+        },
+    ],
+    
+    deps:[
+        'controller/websiteController',
+        'controller/newWebsiteController',
+        'controller/editWebsiteController',
+        'controller/wikiEditorController',
+        'controller/gitVersionController',
+        'controller/homeController',
+        'controller/loginController',
+        'controller/previewController',
+        'controller/siteshowController',
+        'controller/userCenterController',
+
+        // directives
+        //'directive/moduleDirective',
+
+        // factory
+        //'factory/account',
+    ]
+})
--- start.sh
+++ start.sh
@@ -1,4 +1,104 @@
-workDir=`pwd`
-#npl -d bootstrapper="script/apps/WebServer/WebServer.lua" ip="10.0.2.15" port="8099" root="www/" dev="${workDir}"
-npl -d bootstrapper="script/apps/WebServer/WebServer.lua"  root="www/" port="8099" dev="${workDir}"
+#!/bin/bash
 
+current_dir=`pwd`
+rls_dir="rls"
+test_dir="test"
+dev_dir="www"
+build_dir="www_build"
+
+#test_server() {
+#	echo $current_dir
+#	local db_dir=${build_dst_dir}"/wiki/database"
+#	local temp_db_dir=".database"
+#
+#	if [ -e ${build_dst_dir} -a -e ${db_dir} ]; then 
+#		echo "back up database ..."
+#		cp -fr  ${db_dir} ${temp_db_dir}
+#	fi
+#	#node r.js -o test.js
+#	ls ${db_dir}
+#	rm -fr ${db_dir}
+#	mv ${temp_db_dir} ${db_dir}
+#}
+
+start_server() {
+	local server_type=$1
+	
+	if [ $server_type = "test" ]; then
+		rm -fr ${test_dir}
+		cp -fr ${build_dir} ${test_dir}
+		npl -d bootstrapper="script/apps/WebServer/WebServer.lua"  root="${test_dir}/" port="8099" logfile="${test_dir}_log.log" 
+	elif [ $server_type = "rls" ]; then 
+		rm -fr ${rls_dir}
+		cp -fr ${build_dir} ${rls_dir}
+		npl -d bootstrapper="script/apps/WebServer/WebServer.lua"  root="${rls_dir}/" port="8088" logfile="${rls_dir}_log.log"
+	elif [ $server_type = "dev" ]; then 
+		npl -d bootstrapper="script/apps/WebServer/WebServer.lua"  root="${dev_dir}/" port="8900" logfile="${dev_dir}_log.log"
+	else
+		start_server "test"
+		start_server "dev"
+	fi
+}
+
+stop_server() {
+	local server_type=$1
+
+	if [ $server_type = "test" ]; then
+		pid=`ps uax | grep "npl.*port=8099.*" | grep -v grep | awk '{print $2}'`
+		if [ ! -z $pid ]; then
+			kill -9 $((pid))
+		fi
+	elif [ $server_type = "dev" ]; then 
+		pid=`ps uax | grep "npl.*port=8900.*" | grep -v grep | awk '{print $2}'`
+		if [ ! -z $pid ]; then
+			kill -9 $((pid))
+		fi
+	elif [ $server_type = "rls" ]; then 
+		pid=`ps uax | grep "npl.*port=8088.*" | grep -v grep | awk '{print $2}'`
+		if [ ! -z $pid ]; then
+			kill -9 $((pid))
+		fi
+	else
+		stop_server "test"
+		stop_server "dev"
+	fi
+}
+
+restart_server() {
+	local server_type=$1
+
+	stop_server $server_type
+	start_server $server_type
+}
+
+
+main() {
+	echo "------------main function----------------"
+	
+	local server_type=$2
+	if [ -z $server_type  ]; then 
+		server_type="all"
+	fi
+	if [ "$1" == "build" ]; then
+		# 打包  ./start.sh build
+		if [ -e ${build_dir} ]; then
+			rm -fr ${build_dir}
+		fi
+		node r.js -o r_package.js
+	elif [ "$1" == "start" ]; then
+		# etc:  ./start.sh start dev|test|rls
+		echo "start server :"$server_type
+		start_server $server_type
+	elif [ "$1" == "stop" ]; then
+		# etc:  ./start.sh stop dev|test|rls
+		echo "stop server :"$server_type  
+		stop_server $server_type
+	else
+		# etc:  ./start.sh restart dev|test|rls
+		# $server_type为空 重启 dev 和 test  不重启rls
+		echo "restart server :"$server_type
+		restart_server $server_type
+	fi
+}
+
+main $1 $2
