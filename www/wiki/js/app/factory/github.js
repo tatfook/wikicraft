6540674116caad501096ac46ca3ee5a2eb48f188
wxaxiaoyao <765485868@qq.com>
2017-06-01 18:03:00 +0800

    xxx

--- www/wiki/js/app/factory/github.js
+++ www/wiki/js/app/factory/github.js
@@ -26,8 +26,8 @@ define([
         github.httpRequest = function (method, url, data, cb, errcb) {
             var config = {
                 method: method,
-                url: github.apiBaseUrl + url,
-                headers: github.defaultHttpHeader,
+                url: this.apiBaseUrl + url,
+                headers: this.defaultHttpHeader,
                 skipAuthorization: true,  // 跳过插件satellizer认证
             };
             if (method == "POST" || method == "PUT") {
@@ -46,40 +46,41 @@ define([
         }
         // user operation
         github.getUser = function (cb, errcb) {
-            github.httpRequest('GET', '/user', {}, cb, errcb);
+            this.httpRequest('GET', '/user', {}, cb, errcb);
         };
 
         // repos operation
         // listRepos
         github.listRepos = function (cb, errcb) {
             var url = '/user/repos';
-            github.httpRequest("GET", url, {affiliation: 'owner'}, cb, errcb);
+            this.httpRequest("GET", url, {affiliation: 'owner'}, cb, errcb);
         };
 
         github.getRepos = function (cb, errcb) {
-            var url = '/repos/' + github.githubName + '/' + github.defaultRepoName;
-            github.httpRequest('GET', url, {}, cb, errcb);
+            var url = '/repos/' + this.githubName + '/' + this.defaultRepoName;
+            this.httpRequest('GET', url, {}, cb, errcb);
         };
 
         // createRespo
         github.createRepos = function (cb, errcb) {
-            github.httpRequest("POST", "/user/repos", {name: github.defaultRepoName}, cb, errcb);
+            this.httpRequest("POST", "/user/repos", {name: this.defaultRepoName}, cb, errcb);
         };
 
         // delete repos
         github.deleteRepos = function (cb, errcb) {
-            var url = "/repos/" + github.githubName + '/' + github.defaultRepoName;
-            github.httpRequest("DELETE", url, {}, cb, errcb);
+            var url = "/repos/" + this.githubName + '/' + this.defaultRepoName;
+            this.httpRequest("DELETE", url, {}, cb, errcb);
         };
 
         // 设置默认库
         github.setDefaultRepo = function (repoName, cb, errcb) {
-            github.defaultRepoName = repoName || 'keepwork_datasource';
+            var self = this;
+            self.defaultRepoName = repoName || 'keepwork_datasource';
             //console.log(storage.sessionStorageGetItem('githubRepoExist'));
             // 会话期记录是否已存在数据源库，避免重复请求
-            var repoKey = 'githubRepoExist_' + github.defaultRepoName;
+            var repoKey = 'githubRepoExist_' + self.defaultRepoName;
             if (!storage.sessionStorageGetItem(repoKey)) {
-                github.getRepos(function (data) {
+                self.getRepos(function (data) {
                     storage.sessionStorageSetItem(repoKey, true);
                     cb && cb(data);
                 }, function (response) {
@@ -87,7 +88,7 @@ define([
                         errcb && errcb(response);
                         return;
                     }
-                    github.createRepos(function (data) {
+                    self.createRepos(function (data) {
                         storage.sessionStorageSetItem(repoKey, true);
                         cb && cb(data);
                     }, errcb);
@@ -100,13 +101,13 @@ define([
         // content operations
         // actions: CREATE UPDATE READ DELETE
         github.fileCURD = function (method, data, cb, errcb) {
-            var url = '/repos/' + github.githubName + '/' + github.defaultRepoName + '/contents/' + data.path;
-            github.httpRequest(method, url, data, cb, errcb);
+            var url = '/repos/' + this.githubName + '/' + this.defaultRepoName + '/contents/' + data.path;
+            this.httpRequest(method, url, data, cb, errcb);
         };
 
         // rollbackFile
         github.rollbackFile = function (ref, path, message, cb, errcb) {
-            var self = github;
+            var self = this;
             var data = {ref: ref, path: path}
 
             self.getFile(data, function (file) {
@@ -119,13 +120,14 @@ define([
 
         // tree
         github.getTree = function (params, cb, errch) {
-            var path = github.getLongPath(params);
+            var self = this;
+            var path = self.getLongPath(params);
             var recursive = params.recursive == undefined ? true : params.recursive;
             var sitename = path.substring(path.lastIndexOf('/') + 1);
             var sha = undefined;
 
             //console.log(path, sitename, path.substring(0, path.lastIndexOf('/')));
-            github.fileCURD("GET", {path:path.substring(0, path.lastIndexOf('/'))}, function (data) {
+            self.fileCURD("GET", {path:path.substring(0, path.lastIndexOf('/'))}, function (data) {
                 //console.log(data, sitename, path);
                 for (var i = 0; i < data.length; i++) {
                     if (data[i].type == "dir" && data[i].name == sitename){
@@ -139,9 +141,9 @@ define([
                     return;
                 }
 
-                var url = '/repos/' + github.githubName + '/' + github.defaultRepoName + '/git/trees/'+ sha + (recursive ? '?recursive=1' : '');
+                var url = '/repos/' + self.githubName + '/' + self.defaultRepoName + '/git/trees/'+ sha + (recursive ? '?recursive=1' : '');
                 var _path = path;
-                github.httpRequest('GET', url, {}, function(data) {
+                self.httpRequest('GET', url, {}, function(data) {
                     data = data.tree;
                     var pagelist = [];
                     //console.log(data);
@@ -153,7 +155,7 @@ define([
                         if (suffixIndex < 0)
                             continue;
 
-                        page.url = path.substring(github.rootPath.length, path.lastIndexOf('.'));
+                        page.url = path.substring(self.rootPath.length, path.lastIndexOf('.'));
                         var paths = page.url.split('/');
                         if (paths.length < 3)
                             continue;
@@ -172,40 +174,40 @@ define([
 
         // commit 
         github.listCommits = function (data, cb, errcb) {
-            var url = '/repos/' + github.githubName + '/' + github.defaultRepoName + '/commits';
-            github.httpRequest('GET', url, data, cb, errcb);
+            var url = '/repos/' + this.githubName + '/' + this.defaultRepoName + '/commits';
+            this.httpRequest('GET', url, data, cb, errcb);
         };
 
         github.getSingleCommit = function (sha, cb, errcb) {
-            var url = '/repos/' + github.githubName + '/' + github.defaultRepoName + '/commits/' + sha;
-            github.httpRequest('GET', url, {}, cb, errcb);
+            var url = '/repos/' + this.githubName + '/' + this.defaultRepoName + '/commits/' + sha;
+            this.httpRequest('GET', url, {}, cb, errcb);
         };
 
         github.getLongPath = function (params) {
-            return github.rootPath + (params.path || "");
+            return this.rootPath + (params.path || "");
         }
 
         // 设置lastCommitId
         github.setLastCommitId = function (lastCommitId) {
-            github.lastCommitId = lastCommitId;
+            this.lastCommitId = lastCommitId;
         }
 
         github.init = function (dataSource, cb, errcb) {
-            var self = github;
-            if (github.inited) {
+            var self = this;
+            if (self.inited) {
                 cb && cb();
                 return;
             }
 
-            github.type = dataSource.type || "github";
-            github.githubToken = dataSource.dataSourceToken;
-            github.githubName = dataSource.dataSourceUsername;
-            github.defaultRepoName = dataSource.projectName || github.defaultRepoName;
-            github.defaultHttpHeader['Authorization'] = ' token ' + github.githubToken;
-            github.apiBaseUrl = dataSource.apiBaseUrl;
-            github.rawBaseUrl = dataSource.rawBaseUrl || 'https://raw.githubusercontent.com';
-            github.rootPath = dataSource.rootPath || '';
-            github.lastCommitId = dataSource.lastCommitId || "master";
+            self.type = dataSource.type || "github";
+            self.githubToken = dataSource.dataSourceToken;
+            self.githubName = dataSource.dataSourceUsername;
+            self.defaultRepoName = dataSource.projectName || self.defaultRepoName;
+            self.defaultHttpHeader['Authorization'] = ' token ' + self.githubToken;
+            self.apiBaseUrl = dataSource.apiBaseUrl;
+            self.rawBaseUrl = dataSource.rawBaseUrl || 'https://raw.githubusercontent.com';
+            self.rootPath = dataSource.rootPath || '';
+            self.lastCommitId = dataSource.lastCommitId || "master";
 
             if (!dataSource.dataSourceUsername || !dataSource.dataSourceToken || !dataSource.apiBaseUrl || !dataSource.rawBaseUrl) {
                 console.log("github data source init failed!!!");
@@ -217,7 +219,7 @@ define([
                 var hookUrl = config.apiUrlPrefix + "data_source/githubWebhook";
                 //var hookUrl = "http://dev.keepwork.com/api/wiki/models/data_source/githubWebhook";
                 var isExist = false;
-                github.httpRequest("GET","/repos/"+github.githubName + "/" + github.defaultRepoName + "/hooks",{}, function (data) {
+                self.httpRequest("GET","/repos/"+self.githubName + "/" + self.defaultRepoName + "/hooks",{}, function (data) {
                     //console.log(data);
                     for (var i = 0; i < data.length; i++) {
                         if (data[i].config.url == hookUrl && data[i].name == "web" && data[i].active) {
@@ -225,7 +227,7 @@ define([
                         }
                     }
                     if (!isExist) {
-                        github.httpRequest("POST","/repos/"+github.githubName + "/" + github.defaultRepoName + "/hooks",{
+                        self.httpRequest("POST","/repos/"+self.githubName + "/" + self.defaultRepoName + "/hooks",{
                             name:"web",
                             active:true,
                             events:["push"],
@@ -247,14 +249,14 @@ define([
                 if (dataSource.projectName) {
                     return;
                 }
-                dataSource.projectName = github.defaultRepoName;
+                dataSource.projectName = self.defaultRepoName;
                 util.post(config.apiUrlPrefix + "data_source/upsert", dataSource);
             }
 
             var getLastCommitId = function (cb, errcb) {
-                github.listCommits({}, function (data) {
+                self.listCommits({}, function (data) {
                     if (data && data.length > 0) {
-                        github.lastCommitId = data[0].sha;
+                        self.lastCommitId = data[0].sha;
                     }
                     cb && cb();
                 }, errcb)
@@ -265,46 +267,48 @@ define([
                 updateDataSource();
             }
 
-            self.setDefaultRepo(github.defaultRepoName, function (data) {
-                github.inited = true;
+            self.setDefaultRepo(self.defaultRepoName, function (data) {
+                self.inited = true;
                 successCallback();
                 getLastCommitId(cb, errcb);
             }, errcb);
         }
 
         github.isInited = function () {
-            return github.inited;
+            return this.inited;
         }
 
         github.getDataSourceType = function () {
-            return github.type;
+            return this.type;
         }
         github.getContentUrlPrefix = function (params) {
-            return github.apiBaseUrl + '/' + github.githubName + '/' + github.defaultRepoName + '/blob/' + github.lastCommitId + github.getLongPath(params);
+            return this.apiBaseUrl + '/' + this.githubName + '/' + this.defaultRepoName + '/blob/' + this.lastCommitId + this.getLongPath(params);
         }
 
         github.getRawContentUrlPrefix = function (params) {
-            return github.rawBaseUrl + '/' + github.githubName + '/' + github.defaultRepoName + '/' + github.lastCommitId + github.getLongPath(params);
+            return this.rawBaseUrl + '/' + this.githubName + '/' + this.defaultRepoName + '/' + this.lastCommitId + this.getLongPath(params);
         }
 
         // writeFile
         github.writeFile = function (params, cb, errcb) {
+            var self = this;
             params.content = Base64.encode(params.content);
             params.message = params.message || "keepwork commit";
-            params.path = github.getLongPath(params).substring(1);
-            github.fileCURD("GET",{path: params.path}, function (result) {
+            params.path = self.getLongPath(params).substring(1);
+            self.fileCURD("GET",{path: params.path}, function (result) {
                 //console.log(result);
                 params.sha = result.sha;
-                github.fileCURD('PUT', params, cb, errcb);
+                self.fileCURD('PUT', params, cb, errcb);
             }, function () {
-                github.fileCURD('PUT', params, cb, errcb);
+                self.fileCURD('PUT', params, cb, errcb);
             });
         }
         // read file
         github.getFile = function (params, cb, errcb) {
-            params.path = github.getLongPath(params).substring(1);
-            params.ref = github.lastCommitId;
-            github.fileCURD("GET", {path:params.path}, function (data) {
+            var self = this;
+            params.path = self.getLongPath(params).substring(1);
+            params.ref = self.lastCommitId;
+            self.fileCURD("GET", {path:params.path}, function (data) {
                 //console.log(data)
                 data.content = data.content && Base64.decode(data.content);
                 cb && cb(data);
@@ -312,24 +316,28 @@ define([
         };
         // deleteFile
         github.deleteFile = function (params, cb, errcb) {
+            var self = this;
+
             params.message = params.message || "keepwork commit";
-            params.path = github.getLongPath(params).substring(1);
-            github.fileCURD("GET", {path:params.path}, function (result) {
+            params.path = self.getLongPath(params).substring(1);
+            self.fileCURD("GET", {path:params.path}, function (result) {
                 data.sha = result.sha;
-                github.fileCURD("DELETE", data, cb, errcb);
+                self.fileCURD("DELETE", data, cb, errcb);
             });
         };
 
         // params: path
         github.getContent = function (params, cb, errcb) {
-            github.getFile(params, function (data) {   // github.getFile 已做 base64解码
+            this.getFile(params, function (data) {   // github.getFile 已做 base64解码
                 cb && cb(data.content);
             }, errcb)
         }
         
         github.getRawContent = function (params, cb, errcb) {
+            var self = this;
+
             var _getRawContent = function () {
-                var url = github.getRawContentUrlPrefix(params);
+                var url = self.getRawContentUrlPrefix(params);
                 $http({
                     method: 'GET',
                     url: url,
@@ -362,6 +370,7 @@ define([
 
         github.uploadImage = function (params, cb, errcb) {
             //params path, content
+            var self = this;
             var path = params.path;
             var content = params.content;
             if (!path) {
@@ -384,17 +393,17 @@ define([
             }
 
             var data = {
-                path: github.getLongPath({path:path}).substring(1),
+                path: self.getLongPath({path:path}).substring(1),
                 message:"keepwork upload image:" + path,
                 content: content
             };
-            github.fileCURD("GET", {path:data.path}, function (result) {
+            self.fileCURD("GET", {path:data.path}, function (result) {
                 data.sha = result.sha;
-                github.fileCURD('PUT', data, function(data){
+                self.fileCURD('PUT', data, function(data){
                     cb && cb(data.content.download_url);
                 }, errcb);
             }, function () {
-                github.fileCURD('PUT', data, function(data){
+                self.fileCURD('PUT', data, function(data){
                     cb && cb(data.content.download_url);
                 }, errcb);
             });
