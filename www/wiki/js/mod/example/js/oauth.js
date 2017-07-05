3c75ff59af28a782c99941466c5fe4e9c43b511d
wxaxiaoyao <765485868@qq.com>
2017-07-05 17:03:24 +0800

    xxx

---
+++ www/wiki/js/mod/example/js/oauth.js
@@ -0,0 +1,48 @@
+/**
+ * Created by wuxiangan on 2017/1/6.
+ */
+
+define([
+    'app',
+    'helper/util',
+    'text!wikimod/example/html/oauth.html',
+], function (app, util,  htmlContent) {
+	function registerController(wikiBlock) {
+		app.registerController("exampleOautController", ['$scope','$http','$auth', function ($scope, $http, $auth) {
+			function init() {
+				$scope.goOauth = function(){
+					console.log("client redirect auth page");
+					//var redirect_uri = encodeURIComponent("http://localhost:8900/wiki/login");
+					var redirect_uri = "http://localhost:8900/wiki/oauth";
+					util.go("oauth?response_type=code&client_id=1000000&redirect_uri="+redirect_uri+"&scope=login&state=test");
+					return ;
+				}
+
+				$scope.standardOauth = function() {
+					$auth.authenticate("keepwork").then(function(response){
+						console.log(response);
+						console.log(response.data);
+					}, function(response){
+						console.log(response);
+					});
+				}
+
+			}
+			$scope.$watch("$viewContentLoaded", init);
+		}]);
+	}
+    
+    return {
+        render: function (wikiBlock) {
+            registerController(wikiBlock);
+            return  htmlContent;       // 返回模块标签内容
+        }
+    }
+});
+
+/*
+ ```@example/js/oauth
+ {
+ }
+ ```
+ */
---
+++ www/wiki/js/mod/example/js/swiper.js
@@ -0,0 +1,42 @@
+/**
+ * Created by wuxiangan on 2017/1/9.
+ */
+
+define(['app', 'text!wikimod/example/html/swiper.html'], function (app, htmlContent) {
+    function registerController(wikiBlock) {
+        app.registerController("swiperController", ['$scope',function ($scope) {
+			$scope.$watch("$viewContentLoaded", function(){
+				console.log("-----------------");
+				var mySwiper = new Swiper ('.swiper-container', {
+					// Optional parameters
+				   	direction: 'vertical',
+				  	loop: true,
+				   
+				   	// If we need pagination
+				   	pagination: '.swiper-pagination',
+				   
+				   	// Navigation arrows
+				   	nextButton: '.swiper-button-next',
+				   	prevButton: '.swiper-button-prev',
+				   
+				   	// And if we need scrollbar
+				   	scrollbar: '.swiper-scrollbar',
+				 });
+			});
+        }]);
+    }
+
+    return {
+        render: function (wikiBlock) {
+            registerController(wikiBlock);
+            return  htmlContent;       // 返回模块标签内容
+        }
+    }
+});
+
+/*
+ ```@example/js/swiper
+ {
+ }
+ ```
+ */
