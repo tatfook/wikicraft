436967b596188f5ef4a7cc715de215892e5d9046
onedou <onedou@126.com>
2017-07-06 17:28:19 +0800

    initialization pay

---
+++ www/wiki/js/app/controller/payController.js
@@ -0,0 +1,15 @@
+﻿/**
+ * Created by big on 2017/7/6.
+ */
+
+define([
+    'app',
+    'helper/util',
+    'text!html/pay.html'
+], function (app, util, htmlContent) {
+    app.registerController("payController", ['$scope', 'Account', 'modal', function ($scope, Account, modal) {
+
+    }]);
+
+    return htmlContent;
+});
