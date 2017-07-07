de6073be30ae1182eb9242b2ce279759c2c385d5
ZhangKaitlyn <1873070425@qq.com>
2017-07-07 18:35:09 +0800

    404调整

--- www/wiki/js/app/controller/notfoundController.js
+++ www/wiki/js/app/controller/notfoundController.js
@@ -8,13 +8,49 @@ define([
     'helper/storage',
     'text!html/notfound.html'
 ], function (app, util, storage, htmlContent) {
-    app.controller('notfoundController', ['$scope', 'Account','Message', function ($scope, Account, Message) {
-        $scope.goHome=function () {
-            util.go("home");
-        };
-        
-        function init() {
+    app.controller('notfoundController', ['$scope', '$rootScope', 'Account','Message', function ($scope, $rootScope, Account, Message) {
+        $scope.showNewPageBtn = false;
 
+        $scope.goBack = function () {
+            history.back(-1);
+        };
+
+        $scope.newPage = function () {
+            storage.sessionStorageSetItem("urlObj", util.parseUrl());
+            console.log(storage.sessionStorageGetItem("urlObj"));
+            util.go("wikieditor");
+        };
+
+        function init() {
+            if ($scope.user){
+                var memberName = $scope.user.username;
+                $scope.siteinfo = $rootScope.urlObj;
+                var sitename = $rootScope.urlObj.sitename;
+                var username = $rootScope.urlObj.username;
+
+                if (memberName == username){//访问自己网站不存在的页面，且该网站存在，显示创建页面按钮
+                    util.http('POST', config.apiUrlPrefix + 'website/getByName', {username: username, websiteName: sitename}, function (data) {
+                        if (data) {
+                            $scope.showNewPageBtn = true;
+                        }
+                        console.log(data);
+                    });
+                }else{ //访问他人网站且网站存在，有编辑权限，显示创建页面按钮
+                    console.log(sitename);
+                    util.post(config.apiUrlPrefix + "site_user/getSiteByName", {
+                        memberName: memberName,
+                        username: username,
+                        sitename: sitename
+                    }, function(data){
+                        console.log(data);
+                        if (data){
+                            $scope.showNewPageBtn = true;
+                        }
+                    }, function (err) {
+                        console.log(err);
+                    });
+                }
+            }
         }
         $scope.$watch('$viewContentLoaded', init);
     }]);
