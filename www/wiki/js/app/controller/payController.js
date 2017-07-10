87d30f7e76b8677b37b01b71e99f8adddbe89a53
onedou <onedou@126.com>
2017-07-10 12:03:46 +0800

    update pay js

--- www/wiki/js/app/controller/payController.js
+++ www/wiki/js/app/controller/payController.js
@@ -12,7 +12,8 @@ define([
         
         var queryArgs = util.getQueryObject();
 
-        $scope.price = 1;
+        $scope.price  = 1;
+        $scope.qr_url = "";
 
         if (Account.ensureAuthenticated()) {
             Account.getUser(function (userinfo) {
@@ -22,11 +23,19 @@ define([
         }
 
         $scope.alipay = function () {
-
+            if ($scope.isMobile) {
+                $scope.alipayClient();
+            } else {
+                $scope.alipayQR();
+            }
         }
 
         $scope.wechat = function () {
-
+            if ($scope.isMobile) {
+                $scope.wechatClient();
+            } else {
+                $scope.wechatQR();
+            }
         }
 
         $scope.alipayClient = function () {
@@ -54,7 +63,19 @@ define([
         }
 
         $scope.alipayQR = function () {
+            var params = {
+                "amount": $scope.price,
+                "channel": "alipay_qr",
+                "subject": "支付测试",
+                "body": "支付测试",
+            };
 
+            createCharge(params, function (charge) {
+                //console.log(charge);
+                $scope.qr_url = charge.credential.alipay_qr;
+
+                $(".pay-qrcode").css("display", "block");
+            })
         }
 
         $scope.wechatQR = function () {
@@ -66,9 +87,33 @@ define([
             };
 
             createCharge(params, function (charge) {
-                console.log(charge);
+                //console.log(charge);
+                $scope.qr_url = charge.credential.wx_pub_qr;
+
+                $(".pay-qrcode").css("display", "block");
             })
         }
+
+        $scope.isMobile = function () {
+            var u = navigator.userAgent;
+            var isMobile = false;
+
+            console.log(u);
+
+            if (u.match(/ipad/i) && u.match(/ipad/i).toString().toLocaleLowerCase() == "ipad") {
+                isMobile = true;
+            }
+
+            if (u.match(/iphone/i) && u.match(/iphone/i).toString().toLocaleLowerCase() == "iphone") {
+                isMobile = true;
+            }
+
+            if (u.match(/android/i) && u.match(/android/i).toString().toLocaleLowerCase() == "android") {
+                isMobile = true;
+            }
+
+            return isMobile;
+        }();
 
         function createCharge(params, callback) {
             util.http("POST", config.apiUrlPrefix + "pay/createCharge", params, function (response) {
