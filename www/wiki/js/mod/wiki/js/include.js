08f2de7f03177e403a91ba58281a56a7451964fd
wxaxiaoyao <765485868@qq.com>
2017-07-11 15:16:14 +0800

    optimize include wiki cmd

--- www/wiki/js/mod/wiki/js/include.js
+++ www/wiki/js/mod/wiki/js/include.js
@@ -1,71 +1,80 @@
 
 define([
-    'app',
-    'helper/util',
-    'helper/dataSource',
-    'helper/markdownwiki',
-    'text!wikimod/wiki/html/include.html',
+		'app',
+		'helper/util',
+		'helper/dataSource',
+		'helper/markdownwiki',
+		'text!wikimod/wiki/html/include.html',
 ], function (app, util, dataSource, markdownwiki, htmlContent) {
 
-    // 使用闭包使模块重复独立使用
-    function registerController(wikiblock) {
-        // 比赛类活动奖励控制器
-        app.registerController("includeController", ['$scope', function ($scope) {
-            $scope.imgsPath = config.wikiModPath + 'wiki/assets/imgs/';
+	// 使用闭包使模块重复独立使用
+	function registerController(wikiblock) {
+		// 比赛类活动奖励控制器
+		app.registerController("includeController", ['$scope', function ($scope) {
+			$scope.imgsPath = config.wikiModPath + 'wiki/assets/imgs/';
 			$scope.containerId = wikiblock.containerId + "_include";
 
-            var modParams = angular.copy(wikiblock.modParams || {});
+			var modParams = angular.copy(wikiblock.modParams || {});
 			var mdwiki = markdownwiki({editorMode: true, breaks: true});
 			var pageinfo = config.services.$rootScope.pageinfo;
-            function init() {
+
+			// 渲染文本
+			function renderText(text) {
+				util.html("#" + $scope.containerId, mdwiki.render(text));
+			}
+
+			// 渲染url引用内容
+			function renderUrl(contentUrl) {
+				var username = modParams.username || pageinfo.username;
+				var sitename = modParams.sitename || pageinfo.sitename;
+				var currentDataSource = dataSource.getDataSource(username, sitename);
+				var path = contentUrl + config.pageSuffixName;
+				if (path[0] != "/") {
+					path = "/" + username + "/" + sitename + "/" + path;
+				}
+				currentDataSource.getRawContent({path: path, isShowLoading:false}, function (content) {
+					util.html("#" + $scope.containerId, mdwiki.render(content));
+				});
+			}
+			function init() {
 				//console.log($("#" + $scope.containerId));
 				setTimeout(function() {
-					if (modParams.content) {
-						var content = mdwiki.render(modParams.content);
-						//console.log(content);
-						util.html("#" + $scope.containerId, mdwiki.render(modParams.content));
+					if (typeof(modParams) == "string") {
+						renderUrl(modParams);
+					} else if (modParams.content) {
+						renderText(modParams.content);
 					} else if (modParams.contentUrl) {
-						var contentUrl = modParams.contentUrl;
-						var username = modParams.username || pageinfo.username;
-						var sitename = modParams.sitename || pageinfo.sitename;
-						var currentDataSource = dataSource.getDataSource(username, sitename);
-						var path = contentUrl + config.pageSuffixName;
-						if (path[0] != "/") {
-							path = "/" + username + "/" + sitename + "/" + path;
-						}
-						currentDataSource.getRawContent({path: path, isShowLoading:false}, function (content) {
-							util.html("#" + $scope.containerId, mdwiki.render(content));
-						});
+						renderUrl(modParams.contentUrl);
 					}
 				});
 			}
 
-            $scope.$watch("$viewContentLoaded", init);
-        }]);
-    }
+			$scope.$watch("$viewContentLoaded", init);
+		}]);
+	}
 
-    return {
-        render: function (wikiblock) {
-            registerController(wikiblock);
-            return htmlContent;
-        }
-    }
+	return {
+		render: function (wikiblock) {
+			registerController(wikiblock);
+			return htmlContent;
+		}
+	}
 });
 
 /*
    ```@wiki/js/include
    {
-		"content":"#test",
+   "content":"#test",
    }
    ```
    ```@wiki/js/include
    {
-		"contentUrl":"test"
+   "contentUrl":"test"
    }
    ```
    ```@wiki/js/include
    {
-		"contentUrl":"/xiaoyao/xiaoyao/test"
+   "contentUrl":"/xiaoyao/xiaoyao/test"
    }
    ```
-*/
+   */
