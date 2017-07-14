f6e59429ab1c91df03ac31a2dc3f76e5e7787705
wxaxiaoyao <765485868@qq.com>
2017-07-14 09:56:29 +0800

    xxx

--- dev_env.sh
+++ dev_env.sh
@@ -5,7 +5,7 @@ code_dir="../keepwork_code"
 change_dev_env() {
 	local env_branch=$1
 	local env_name=$2
-	if [ -z $env_branch or -z $env_name ]; then
+	if [ -z "$env_branch" -o -z "$env_name" ]; then
 		echo "需要指明环境类型(分支名 环境名)"
 	fi
 
