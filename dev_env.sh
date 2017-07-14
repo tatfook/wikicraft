5aee189e99fd934ac801096eefb4374422d3daf3
wxaxiaoyao <765485868@qq.com>
2017-07-14 10:09:18 +0800

    xxx

--- dev_env.sh
+++ dev_env.sh
@@ -7,16 +7,18 @@ change_dev_env() {
 	local env_name=$2
 	if [ -z "$env_branch" -o -z "$env_name" ]; then
 		echo "需要指明环境类型(分支名 环境名)"
+		return
 	fi
 
+	bash start.sh stop dev
 	cd $code_dir
-	git reset --hard HEAD
+	git reset --hard head
 	git checkout $env_branch
 	git pull origin $env_branch
 	cd -
 	sh start.sh stop dev
 	rm -fr "www/$env_name"
-	cp -fr "$code_dir/wwww/wiki" "www/$env_name"
+	cp -fr "$code_dir/www/wiki" "www/$env_name"
 	sh start.sh start dev
 }
 
