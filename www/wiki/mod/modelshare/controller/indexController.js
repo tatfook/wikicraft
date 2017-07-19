20a0814c6004b2208f5c47d45d53a30ca0802656
onedou <onedou@126.com>
2017-07-19 16:40:08 +0800

    modelshare training

--- www/wiki/mod/modelshare/controller/indexController.js
+++ www/wiki/mod/modelshare/controller/indexController.js
@@ -11,48 +11,4 @@
     }]);
     
     return htmlContent;
-    })
-
-
-
-
-
-
-
-
-/*function newFunction() {
-    x = document.getElementById("form");
-    n = document.getElementById("newWorks");
-    h = document.getElementById("hotWorks");
-    a = document.getElementById("act")
-    // 找到元素
-    x.style.display = "none";
-    n.style.display = "block";
-    h.style.display = "none";
-    a.style.display = "none";
-    // 改变内容
-}
-function actFunction() {
-    x = document.getElementById("form");
-    n = document.getElementById("newWorks");
-    h = document.getElementById("hotWorks");
-    a = document.getElementById("act")
-    // 找到元素
-    x.style.display = "none";
-    a.style.display = "block";
-    n.style.display = "none";
-    h.style.display = "none";
-    // 改变内容
-}
-function hotFunction() {
-    x = document.getElementById("form");
-    n = document.getElementById("newWorks");
-    h = document.getElementById("hotWorks");
-    a = document.getElementById("act")
-    // 找到元素
-    x.style.display = "none";
-    h.style.display = "block";
-    n.style.display = "none";
-    a.style.display = "none";
-    // 改变内容
-}*/
+})
