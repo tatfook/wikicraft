4210e82dabfaee8349015ac4ce7396cf08bfc2d3
DreamAndDead <favorofife@yeah.net>
2017-08-11 10:54:34 +0800

    add ops files

---
+++ build-test.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+#
+# build-dist.sh
+#
+
+set -ex
+
+# nodejs compress code, only for test env
+# how it work
+# 1 mkdir temp_www_build
+# 2 cp www to temp_www_build
+# 3 node r.js -o r_package.js
+# 4 new js files are placed in www_build dir, as release version
+# 5 remember remove temp_www_build dir
+TEMP_DIR=temp_www_build
+BUILD_DIR=www_build
+DEV_DIR=www
+TEST_DIR=test
+PROD_DIR=prod
+
+clean_dir() {
+  if [[ -d $1 ]] || [[ -L $1 ]]; then
+    rm -rf $1
+  fi
+}
+
+clean_dir $TEMP_DIR
+clean_dir $BUILD_DIR
+
+cp -a $DEV_DIR $TEMP_DIR
+node r.js -o r_package.js
+clean_dir $TEMP_DIR
+
+clean_dir $TEST_DIR
+ln -s $BUILD_DIR $TEST_DIR
+ln -s $BUILD_DIR $PROD_DIR
---
+++ serve.sh
@@ -0,0 +1,48 @@
+#! /usr/bin/env bash
+#
+# serve.sh dev|test|prod
+#
+# serve npl web server
+
+set -x
+
+usage() {
+  echo "usage error"
+  echo
+  echo "usage: $0 dev|test"
+  exit 1
+}
+
+if [[ $# -eq 0 ]] || [[ $# -gt 1 ]]; then
+  usage
+fi
+
+if [[ $1 != "test" ]] && [[ $1 != "dev" ]] && [[ $1 != "prod" ]]; then
+  usage
+fi
+
+ENV_TYPE=$1
+
+case $ENV_TYPE in
+  dev)
+    ROOT_DIR=www
+    PORT=8900
+    ;;
+  test)
+    ROOT_DIR=test
+    PORT=8099
+    ;;
+  prod)
+    ROOT_DIR=prod
+    PORT=8088
+    ;;
+esac
+
+LOG_DIR=./log
+mkdir -p $LOG_DIR
+
+# TODO backup log
+ulimit -c unlimited
+npl -D bootstrapper="script/apps/WebServer/WebServer.lua"  root="$ROOT_DIR/" port="$PORT" logfile="$LOG_DIR/$ENV_TYPE.log"
+
+
