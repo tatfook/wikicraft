b744368ce09680f6daa3f240343fa1d1a7772283
tpp <307926882@qq.com>
2017-08-23 15:02:42 +0800

    查看作品页的api制作

--- www/wiki/mod/modelshare/controller/indexController.js
+++ www/wiki/mod/modelshare/controller/indexController.js
@@ -2,8 +2,16 @@
     'app',
     'text!mod/modelshare/html/index.html',
 ], function (app, htmlContent) {
-    app.controller('indexController', ['$scope', function ($scope) {
+    app.controller('indexController', ['$scope', '$http' , function ($scope,$http) {
         $scope.modName = "example";
+		$http({
+				method: 'GET',
+				url: 'http://localhost:8099/api/mod/modelshare/models/modelshare/text'
+			}).then(function successCallback(response) {
+					$scope.h2h2 = response.data;
+				}, function errorCallback(response) {
+					// 请求失败执行代码
+			});
         $scope.divVar = 4;
         $scope.set = function (params) {
             $scope.divVar = params;
