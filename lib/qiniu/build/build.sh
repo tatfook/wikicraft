dd78fd0437539d863760d4cdb6cfac16342d3142
wxaxiaoyao <765485868@qq.com>
2017-08-25 15:48:35 +0800

    xxx

--- lib/qiniu/build/build.sh
+++ lib/qiniu/build/build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 export NPLRUNTIME_ROOT=/root/workspace/npl/NPLRuntime
+
 rm Makefile
 rm cmake*
 rm -fr CMake*
+
 cmake ~/workspace/npl/nplproject/keepwork/lib/qiniu
 make
+
+rm Makefile
+rm cmake*
+rm -fr CMake*
+
 mv libQiNiuPlugin.so ~/workspace/npl/nplproject/keepwork/lib
