24f0d4ee58e02eb4e66b744176ac3e59ffa0eb94
wxaxiaoyao <765485868@qq.com>
2017-08-25 18:11:54 +0800

    xxx

---
+++ www/wiki/js/mod/wiki/js/iframe.js
@@ -0,0 +1,50 @@
+/**
+ * Created by wuxiangan on 2017/4/24.
+ */
+
+define([
+    'app',
+    'text!wikimod/wiki/html/iframe.html',
+], function (app, htmlContent) {
+    function registerController(wikiBlock) {
+        app.registerController("iframeController", ['$scope', '$sce', "$auth", function ($scope, $sce, $auth) {
+            function init() {
+				var iframeUrl = wikiBlock.modParams.iframeUrl;
+				var iframeId = wikiBlock.modParams.iframeId;
+
+				if (!iframeId || !iframeUrl) {
+					return;
+				}
+
+				if ($auth.isAuthenticated()) {
+					var token = $auth.getToken();
+					//token = encodeURIComponent(token);
+					if (iframeUrl.indexOf("?") > 0) {
+						iframeUrl = iframeUrl + "&token=" + token;
+					} else {
+						iframeUrl = iframeUrl + "?token=" + token;
+					}
+				}
+				$scope.iframeUrl = $sce.trustAsResourceUrl(iframeUrl);
+				//$scope.iframeUrl = iframeUrl;
+                $scope.iframeId = iframeId;
+            }
+
+			$scope.$watch("$viewContentLoaded", init);
+        }]);
+    }
+
+    return {
+        render: function (wikiBlock) {
+            registerController(wikiBlock);
+            return  htmlContent;       // 返回模块标签内容
+        }
+    }
+});
+
+//```
+//{
+	//"iframeId":"iframeId",
+	//"iframeUrl":"http://keepwork.com"
+//}
+//```
