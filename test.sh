39eeaefe3df92f3e24514259d21f535005f577c2
wxaxiaoyao <765485868@qq.com>
2017-08-29 10:27:18 +0800

    xxx

--- test.sh
+++ test.sh
@@ -1,12 +1,1 @@
-
-curtime=`date +%Y-%m-%d_%H-%M-%S`
-
-main() {
-	local typ=$1
-	local curtime=`date +%Y-%m-%d_%H-%M-%S`
-	mkdir "log"
-	cp  "${typ}_log.log" "log/${typ}_log_${curtime}.log"
-	cat "${typ}_log.log" | grep -i "<runtime" >> "log/${typ}_error.log"
-}
-
-main "www"
+echo "\nhello world\n" >> www_log.log
