a5832eac6f7e0635e47e57349be62c4348976a78
wxaxiaoyao <765485868@qq.com>
2017-09-04 16:06:56 +0800

    oauth auto grant

--- www/wiki/js/app/controller/testController.js
+++ www/wiki/js/app/controller/testController.js
@@ -10,56 +10,13 @@ define([
 ], function (app, util, mdconf, htmlContent) {
 	app.registerController("testController", ['$scope','$http','$auth', function ($scope, $http, $auth ) {
 		function init() {
-			var str =`
-# Defaults
-
-  Since this is markdown you can still have regular text
-  to explain what the hell is going on.
-
-## Upload
-
-  - max: 200mb
-  
-  
-  - dir: /tmp
-
-### Thumbnail sizes
-
-  - 50x50
-  - 300x300
-  - 600x600
-  - 900x900
-
-## S3
-
-  - api key: 111111
-  - secret: 222222
-
-### Buckets
-
-  - avatars: myapp-avatars
-  - assets: myapp-assets
-  - files: myapp-files
-
-# Production
-
-## Upload
-
-  - max: 1gb
-  - dir: /data/uploads
-
-## Sites
-
-| hostname     | build   | coverage  |
-| :----------- | :------:| --------: |
-| google.com   | pas\\|sing |       94% |
-| facebook.com | passing |       97% |
-| twitter.com  | failed  |       81% |
-| myspace.com  | unkown  |       0%  |
-
-				`;
-			var obj = mdconf.toJson(str);
-			console.log(mdconf.toMd(obj));
+			config.apiUrlPrefix = "http://xiaoyao.localhost.com:8900/api/wiki/models/";
+			//util.post(config.apiUrlPrefix + "test/cookie",{}, function(data){
+				//console.log(data);
+			//});
+			util.post(config.apiUrlPrefix + "test/getCookie",{}, function(data){
+				console.log(data);
+			});
 		}
 
 		$scope.$watch("$viewContentLoaded", init);
