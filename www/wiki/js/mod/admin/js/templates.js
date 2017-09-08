43b7a407f9de59d585d8d2ed20d41fee7803fc74
ZhangKaitlyn <1873070425@qq.com>
2017-09-08 16:43:16 +0800

    网站模板管理；

--- www/wiki/js/mod/admin/js/templates.js
+++ www/wiki/js/mod/admin/js/templates.js
@@ -5,70 +5,180 @@ define([
     'app',
     'helper/util',
     'helper/storage',
+    'markdown-it',
     'text!wikimod/admin/html/templates.html',
-], function (app, util, storage, htmlContent) {
-    function getModParams(wikiblock) {
-        var modParams = wikiblock.modParams || storage.sessionStorageGetItem("wikiModParams") || {};
-        return angular.copy(modParams);
-    }
+    'js-base64',
+], function (app, util, storage, markdownit, htmlContent) {
+    const GITLAB = {
+        "API_BASE_URL": "http://git.keepwork.com/api/v4",
+        "FILE_PATH": "kaitlyn%2Ftemplates%2Findex%2Emd",
+        "REF": "master",
+        "PRIVATE_TOKEN": "5V78ZVAuFRqE2ZeZjw5X",
+        "ID": "6795"
+    };
 
-    function registerController(wikiBlock) {
-        app.registerController("templatesController", ["$scope","$uibModal", function ($scope, $uibModal) {
-            $scope.modParams = getModParams(wikiBlock);
-            $scope.activeCategories = $scope.modParams.categories ? $scope.modParams.categories[0] : {};
+    app.registerController("templatesController", ["$scope","$uibModal", "$http", "Message", function ($scope, $uibModal, $http, Message) {
+        $scope.categories = [];
+        $scope.newClassify = {
+            "name": "",
+            "classify":"",
+            "templates":[]
+        };
+
+        $scope.newTemplate = {
+            "name":"",
+            "logoUrl":"",
+            "previewUrl":"",
+            "styles":[]
+        };
+        $scope.newStyle = {};
+        $scope.newContent = {};
+        var initCategories = function() {
+            var url = GITLAB.API_BASE_URL + "/projects/" + GITLAB.ID +"/repository/files/" + GITLAB.FILE_PATH;
+            var params = {
+                "ref": GITLAB.REF
+            };
+            var isShowLoading = true;
+            util.http("GET", url, params, function (result) {
+                console.log(result);
+            }, function (result) {
+                if (!result.content){
+                    return;
+                }
+                var content = Base64.decode(result.content),
+                    md = markdownit(),
+                    parserContent = md.parse(content)[0].content || "";
+                try{
+                    $scope.categories = angular.fromJson(parserContent);
+                    $scope.activeCategories = $scope.categories[0];
+                }catch (e){
+                    console.log(e);
+                }
+            }, isShowLoading);
+        };
+
+        var saveWebsiteTemplates = function (message) {
+            console.log(JSON.stringify($scope.categories));
+            var config = {
+                method: "PUT",
+                url: GITLAB.API_BASE_URL + "/projects/" + GITLAB.ID +"/repository/files/" + GITLAB.FILE_PATH,
+                data:{
+                    "branch": GITLAB.REF,
+                    "content": "```\n" + JSON.stringify($scope.categories) + "\n```",
+                    "commit_message": message
+                },
+                headers: {
+                    "PRIVATE-TOKEN": GITLAB.PRIVATE_TOKEN
+                },
+                skipAuthorization: true,  // 跳过插件satellizer认证
+                isShowLoading: false
+            };
+            $http(config).then(function (result) {
+                console.log(result);
+                if (result.status == 200){
+                    Message.info("保存成功");
+                }else{
+                    Message.danger("保存失败，请将控制台的错误截图给程序员");
+                    console.log(result)
+                }
+            }).catch(function (err) {
+                console.log(err);
+            });
+        };
+
+        function init() {
+            console.log("init templates");
+            initCategories();
+        }
+
+        // 选择显示某种网站模板分类
+        $scope.selectCategory = function (category) {
+            $scope.activeCategories = category;
+        };
+        // 添加模板分类
+        $scope.saveClassify = function () {
+            $scope.categories.push($scope.newClassify);
             $scope.newClassify = {
                 "name": "",
                 "classify":"",
                 "templates":[]
             };
-            $scope.newTemplate = {
-                "name":"",
-                "logoUrl":"",
-                "previewUrl":"",
-                "styles":[]
-            };
-            $scope.newStyle = {};
-            $scope.newContent = {};
-            // $scope.categories = $scope.modParams.categories || [];
-            // $scope.templates = $scope.categories[0] ? $scope.categories[0].templates : {};
-            function init() {
-                console.log("init templates");
-            }
-
-            $scope.saveClassify = function () {
-                $scope.modParams.categories = $scope.modParams.categories ? $scope.modParams.categories : [];
-                $scope.modParams.categories.push($scope.newClassify);
-                wikiBlock.applyModParams($scope.modParams);
-            };
-
-            $scope.selectCategory = function (category) {
-                $scope.activeCategories = category;
-            };
-
-            $scope.addStyle = function () {
-                $scope.newTemplate.styles.push($scope.newStyle);
-                $scope.newStyle = {};
-            };
-
-            $scope.addContent = function () {
-                $scope.newStyle.contents = $scope.newStyle.contents ? $scope.newStyle.contents : [];
-                $scope.newStyle.contents.push($scope.newContent);
-                $scope.newContent = {};
-            };
-
-            $scope.addTemplate = function () {
+            saveWebsiteTemplates("addClassify");
+        };
+        // 添加新网站模板
+        $scope.addTemplate = function (index) {
+            console.log(index);
+            if (index >= 0){
+                $scope.activeCategories.templates[index] = $scope.newTemplate;
+                $("#editTemplateModal").modal("toggle");
+            }else{
                 $scope.activeCategories.templates.push($scope.newTemplate);
-                wikiBlock.applyModParams($scope.modParams);
-            };
+                console.log($scope.categories);
+            }
+            saveWebsiteTemplates("addTemplate");
+        };
+        // 给模板添加style
+        $scope.addStyle = function (index) {
+            if (index >= 0){
+                $scope.newTemplate.styles[index] = $scope.newStyle;
+            }else{
+                $scope.newTemplate.styles.push($scope.newStyle);
+            }
+            $scope.newStyle = {};
+        };
+        // 给style添加网页模板
+        $scope.addContent = function (index) {
+            if (index >= 0){
+                $scope.newStyle.contents[index] = $scope.newContent;
+            }else{
+                $scope.newStyle.contents = $scope.newStyle.contents ? $scope.newStyle.contents : [];
+                $scope.newStyle.contents.push($scope.newContent)
+            }
+            $scope.newContent = {};
+        };
 
-            $scope.$watch("$viewContentLoaded", init);
-        }]);
-    }
+        $scope.setValue = function (obj) {
+            $scope[obj.key] = obj.value;
+            $scope[obj.key].index = obj.index;
+        };
 
-    return {
-        render: function (wikiBlock) {
-            registerController(wikiBlock);
-            return htmlContent;
+        // 编辑网站模板
+        $scope.editTemplate = function (template, index) {
+            template.index = index;
+            $scope.newTemplate = template;
+            $("#editTemplateModal").modal("toggle");
+        };
+
+        // 删除网站模板分类
+        $scope.deleteClassify = function (classify, index) {
+            config.services.confirmDialog({
+                "title":"删除提醒",
+                "confirmBtnClass":"btn-danger",
+                "theme":"danger",
+                "content":"确定删除 " + (classify.name) + " 分类？"
+            },function(){
+                $scope.categories.splice(index, 1);
+                saveWebsiteTemplates("deleteClassify");
+            });
+        };
+        // 删除网站模板
+        $scope.deleteTemplate = function (template, index) {
+            config.services.confirmDialog({
+                "title":"删除提醒",
+                "confirmBtnClass":"btn-danger",
+                "theme":"danger",
+                "content":"确定删除 " + (template.name) + " 模板？"
+            },function(){
+                $scope.activeCategories.templates.splice(index, 1);
+                saveWebsiteTemplates("deleteTemplate");
+            });
+        };
+
+        $scope.$watch("$viewContentLoaded", init);
+
+        $scope.goPreviewPage = function (url) {
+            window.open(url);
         }
-    };
+    }]);
+    return htmlContent;
 });
