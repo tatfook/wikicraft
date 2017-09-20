902012a294d0c00e0a78a9b01a2cd78c62d332ed
DreamAndDead <favorofife@yeah.net>
2017-09-20 11:14:27 +0800

    compile qiniu

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,10 @@ RUN mkdir -p /project/wikicraft
 ADD ./build.tar.gz /project/wikicraft/
 WORKDIR /project/wikicraft
 
+# compile qiniu sdk (libboost ver > 1.55)
+RUN apt-update && apt install libboost-all-dev
+RUN chmod u+x ./lib/qiniu/build/build.sh && ./lib/qiniu/build/build.sh 
+
 VOLUME ["/project/wikicraft/database", "/project/wikicraft/log"]
 
 ENTRYPOINT ["./serve.sh"]
--- lib/qiniu/build/build.sh
+++ lib/qiniu/build/build.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-keepwork_dir=/root/wikicraft
-npl_runtime_dir=/root/NPLRuntime
-
-keepwork_dir=/root/workspace/npl/nplproject/keepwork
-npl_runtime_dir=/root/workspace/npl/NPLRuntime
+keepwork_dir=/project/wikicraft
+npl_runtime_dir=/opt/NPLRuntime
 
 export NPLRUNTIME_ROOT="${npl_runtime_dir}"
 
-rm Makefile
-rm cmake*
+rm -rf Makefile
+rm -rf cmake*
 rm -fr CMake*
 
 cmake "${keepwork_dir}/lib/qiniu"
 make
 
-rm Makefile
-rm cmake*
+rm -rf Makefile
+rm -rf cmake*
 rm -fr CMake*
 
 mv libQiNiuPlugin.so "${keepwork_dir}/lib"
