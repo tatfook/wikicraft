9f0d611b9d7081a76d331d3a336070e69e6678cd
wxaxiaoyao <765485868@qq.com>
2017-10-17 16:34:34 +0800

    qiniu build

--- lib/qiniu/build/build.sh
+++ lib/qiniu/build/build.sh
@@ -3,6 +3,9 @@
 keepwork_dir=/project/wikicraft
 npl_runtime_dir=/opt/NPLRuntime
 
+#keepwork_dir=/root/wikicraft
+#npl_runtime_dir=/root/NPLRuntime
+
 #keepwork_dir=/root/workspace/npl/nplproject/keepwork
 #npl_runtime_dir=/root/workspace/npl/NPLRuntime
 
