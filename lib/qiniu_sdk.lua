aeb083a4e30fbc502b3ccfe2af25031d42fc469c
wxaxiaoyao <765485868@qq.com>
2017-11-08 18:50:46 +0800

    qiniu file delete fix

---
+++ lib/qiniu_sdk.lua
@@ -0,0 +1,26 @@
+
+NPL.load("(gl)script/ide/commonlib.lua")
+NPL.load("(gl)script/ide/System/Concurrent/rpc.lua")
+
+local rpc = commonlib.gettable("System.Concurrent.Async.rpc")
+
+rpc:new():init("qiniu_sdk.qiniu_sdk", function(self, params) 
+	if type(params) ~= "table" then
+		return
+	end
+
+	local obj = {}
+
+	obj.cmd = params.cmd
+	obj.bucket = params.bucket
+	obj.domain = params.domain
+	obj.key = params.key
+	obj.expires = params.expires
+
+	NPL.call("lib/libQiNiuPlugin.so", obj)
+
+	log(msg)
+
+	return msg
+end, "lib/qiniu_sdk.lua")
+
