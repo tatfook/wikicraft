2e242109e099b1d36976c9748e243b775a126bb7
wxaxiaoyao <765485868@qq.com>
2017-11-09 15:04:35 +0800

    qiniu safe

--- lib/qiniu/QiNiu.cpp
+++ lib/qiniu/QiNiu.cpp
@@ -24,7 +24,7 @@ char* ParaEngine::CQiNiu::getUploadToken(const char *callback_url, int expires) 
 	Qiniu_Zero(putPolicy);
 	putPolicy.scope = this->m_bucket;
 	putPolicy.callbackUrl = callback_url;
-	putPolicy.callbackBody = "uid=$(x:uid)&size=$(fsize)&bucket=$(bucket)&key=($key)";
+	putPolicy.callbackBody = "uid=$(x:uid)&size=$(fsize)&bucket=$(bucket)&key=$(key)";
 	if (expires > 0) {
 		putPolicy.expires = expires;
 	} else {
