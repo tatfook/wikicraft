bcfb10197b606522863c6c60e1c0cad945432b03
Rango Yuan <yuanoook@foxmail.com>
2017-11-17 19:01:46 +0800

    feat(videoplayer) #966 - /wiki/video_player#?video_id=1

--- www/wiki/js/app/controller/videoPlayerController.js
+++ www/wiki/js/app/controller/videoPlayerController.js
@@ -3,16 +3,37 @@
  */
 
 define([
-    'app'
-], function (app, htmlContent) {
-    app.registerController('videoPlayerController', ['$scope', '$rootScope', '$location', '$http', function ($scope, $rootScope, $location, $http) {
+    'app',
+    'helper/util'
+], function (app, util) {
+    app.registerController('videoPlayerController', ['$scope', '$rootScope', '$location', '$http', '$element', function ($scope, $rootScope, $location, $http, $element) {
         $rootScope.frameFooterExist = false;
         $rootScope.frameHeaderExist = false;
+        var video_id = $location.search().video_id
+        $scope.$watch('$viewContentLoaded', function() {
+            util.get(config.apiUrlPrefix + "bigfile/getDownloadUrlById", {
+                _id: video_id,
+            }, function(data){
+                // $element.find('video').attr('src', 'http://foodsound.qiniudn.com/video/introducing_thinglist.mp4');
+                $element.find('video').attr('src', data);
+            });
+        });
     }]);
 
     return '\
-        <div ng-controller="videoPlayerController">\
-            Let\'s Play Video \
+        <style>\
+            html{height: 100%;}\
+            body{overflow-x: hidden;overflow-y: auto;}\
+            body, #__mainContent__{background:black;display: flex;justify-content: center;align-items: center;width: 100%;height: 100%; min-height: auto !important;}\
+        </style>\
+        <div ng-controller="videoPlayerController" \
+            style="width: 100vw; height: auto; max-height: 100vw;"\
+        >\
+            <video \
+                style="display:block;width:100%;height:auto;max-width:100%;max-height:100%;" \
+                controls \
+                preload="auto" \
+            ></video> \
         </div>\
-    ';
+    ';d
 });
