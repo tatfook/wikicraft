19ae9e5ea95fbac1febf9ed246bb7d252b90af87
onedou <onedou@126.com>
2017-11-22 17:03:03 +0800

    update board.min.js cdn

--- www/wiki/js/mod/board/main.js
+++ www/wiki/js/mod/board/main.js
@@ -3,7 +3,7 @@
     'helper/util',
     'text!wikimod/board/main.html',
     'pako',
-    '/wiki/js/mod/board/board.min.js?bust=1',
+    '/wiki/js/mod/board/board.min.js?bust=2',
 ], function (app, util, htmlContent, pako) {
     jscolor.dir = "/wiki/js/mod/board/assets/images/";
 
