d4fc6d4a61220ca24e1aa6c557e1fe2cb95de876
yangzongze <yangzongze@golaxy.cn>
2017-12-14 17:35:55 +0800

    知识库Mod点击详情打开keepwork链接

--- www/wiki/js/mod/knowledgeLib/js/knowledgeLib.js
+++ www/wiki/js/mod/knowledgeLib/js/knowledgeLib.js
@@ -15,82 +15,66 @@ define([
             function init() {
                 console.log("----------init knowledge Mod---------");
             }
-           
-            var server = "121.14.117.220"; 
+
             var context = getContext();
-            getKnowledgeList(context);
+            getResult(context);
             
             function getContext() {
                 var allDoms = $("div[id^='_mdwiki_content_container_mdwiki']").children();
                 var context = "";
 
                 for (var i = 0; i < allDoms.length; i++) {
-                    var html = allDoms[i].innerHTML;
-                    if (html) {
-                        html = html.replace(/[^\u4e00-\u9fa5]/g,'');
+                    console.log(allDoms[i].hasAttribute('contenteditable'));
+                    if (allDoms[i].hasAttribute('contenteditable')) {
+                        var html = allDoms[i].innerHTML;
                         if (html) {
-                            context = context.concat(html);
+                            html = html.replace(/[^\u4e00-\u9fa5]/g,'');
+                            if (html) {
+                                context = context.concat(html);
+                            }
                         }
                     }
+                    
                 }
                 return context;
             }
             
-            function getKnowledgeList(context) {
+            function getResult(context) {
                 $.ajax({
-                    url:"http://"+server+"/KnowledgeServer/api/knowledgeMod/getKnowledgeList",
+                    url:"http://221.0.111.131:19001/Application/kwsegwords",
+                    type: "post",
+                    data: {
+                        "context": context
+                    },
+                    dataType: "json",
+                    success:function(result) {
+                        var word = result.msg;
+                        getList(word);
+                    }
+                });
+            }
+            
+            function getList(word) {
+                $.ajax({
+                    url:"http://221.0.111.131:19001/Application/baikesearch",
                     type : "post",
                     data: {
-                        "keywords": context,
-                        "type": 2,  //keywords类型，1:词语，2:句子
-                        "highlight": 1 
+                        "keyword": word,
+                        "page": 1,
+                        "highlight": 1,
+                        "flag": 1
                     },
                     dataType: "json",
                     success:function(result){
                         $("#knowledge-list").empty();
                         $(result.data.list).each(function(i, val){
                             var index = "<div class='serial'>"+(i+1)+"</div>";
-                            var content = "<div class='rule-content'><h2><a ng-click='getKnowledgeDetail(\""+val.id+"\")'>"+val.title+"</a></h2><p>"+val.content+"......</p></div>";
+                            var content = "<div class='rule-content'><h2><a target=_blank href="+val.keepwork_url+">"+val.title+"</a></h2><p>" + val.content + "......</p></div>";
                             $("#knowledge-list").append($compile(index+content)($scope));
                         });
                     }
-                });
-            }
-            
-            var lastKnowledgeId;
-            $scope.getKnowledgeDetail = (function(id){
-                $("#knowledge-list").hide();
-                $("#knowledge-detail").show();
-                
-                if (id == lastKnowledgeId)
-                    return;
-                
-                $(".knowledge_title").empty();
-                $(".knowledge_content").empty();
-                
-                $.ajax({
-                    url:"http://"+server+"/KnowledgeServer/api/knowledgeMod/getKnowledgeDetail?jsonpcallback=?",
-                    type : "get",
-                    data: {
-                        "id": id
-                    },
-                    dataType: "jsonp",
-                    jsonp: "jsonpcallback",
-                    success:function(result){
-                        var title = result.data.list[0].title;
-                        var content = result.data.list[0].content;
-                        $(".knowledge_title").text(title);
-                        $(".knowledge_content").html(content);
-                        lastKnowledgeId = id;
-                    }
                 })
-            });
-            
-            $scope.returnList = (function() {
-                $("#knowledge-detail").hide();
-                $("#knowledge-list").show();
-            }); 
-            
+            }
         }]);
     }
 
@@ -107,7 +91,7 @@ define([
 ```@knowledgeLib/js/knowledgeLib
 {
     "moduleKind":"game",
-    "title":"ç¾ç§æ¨è",
+    "title":"百科推荐",
     "rules":[
     ]
  }
