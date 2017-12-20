0658c7986d237737938e53ba6c00cfb46251323c
谭秉丰 <tanbingfeng@tanbingfengdeMac-mini.local>
2017-12-20 14:19:27 +0800

    delete

--- www/wiki/js/mod/wiki/js/title.js
+++ www/wiki/js/mod/wiki/js/title.js
@@ -71,92 +71,10 @@ define([
 						text:"YOUR SUB LINE", // 默认值
 						require: true, // 必填字段 没有使用默认值(默认值得有)
 					},
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-				   	// paragraphs:{
-					// 	is_leaf: true, // 叶子对象默认填true
-					// 	type:"text",   // 地段类型
-					// 	editable:true, // 是否可以编辑
-					// 	is_show:true,  // 可视化是否显示 undefined取值editable
-					// 	name:"段落",   // 表单显示名
-					// 	text:"paragraphs", // 默认值
-					// 	require: true, // 必填字段 没有使用默认值(默认值得有)
-					// },
-				   	// link:{
-					// 	is_leaf: true, // 叶子对象默认填true
-					// 	type:"link",   // 地段类型
-					// 	editable:true, // 是否可以编辑
-					// 	is_show:true,  // 可视化是否显示 undefined取值editable
-					// 	name:"链接",   // 表单显示名
-					// 	text:"href text", // 默认值
-					// 	href:"http://www.baidu.com", // 默认值
-					// 	require: true, // 必填字段 没有使用默认值(默认值得有)
-					// },
-				   	// image:{
-					// 	is_leaf: true, // 叶子对象默认填true
-					// 	type:"link",   // 地段类型
-					// 	editable:true, // 是否可以编辑
-					// 	is_show:true,  // 可视化是否显示 undefined取值editable
-					// 	name:"链接",   // 表单显示名
-					// 	text:"href text", // 默认值
-					// 	href:"http://www.baidu.com", // 默认值
-					// 	require: true, // 必填字段 没有使用默认值(默认值得有)
-					// },
-					
-					// list:{
-					// 	is_leaf: true,
-					// 	type: "list",
-					// 	editable: true,
-					// 	is_show: true,
-					// 	require: true,
-					// 	name:"简单列表",
-					// 	list: [
-					// 		{
-					// 			is_leaf: true, // 叶子对象默认填true
-					// 			type:"link",   // 地段类型
-					// 			editable:true, // 是否可以编辑
-					// 			is_show:true,  // 可视化是否显示 undefined取值editable
-					// 			name:"链接",   // 表单显示名
-					// 			text:"href text", // 默认值
-					// 			href:"http://www.baidu.com", // 默认值
-					// 			require: true, // 必填字段 没有使用默认值(默认值得有)
-					// 		},
-					// 	]
-					// },
-					// object_list:{
-					// 	is_leaf: true,
-					// 	type: "list",
-					// 	editable: true,
-					// 	is_show: true,
-					// 	require: true,
-					// 	name:"对象列表",
-					// 	list: [
-					// 		{
-					// 			link: {
-					// 				is_leaf: true, // 叶子对象默认填true
-					// 				type:"link",   // 地段类型
-					// 				editable:true, // 是否可以编辑
-					// 				is_show:true,  // 可视化是否显示 undefined取值editable
-					// 				name:"链接",   // 表单显示名
-					// 				text:"href text", // 默认值
-					// 				href:"http://www.baidu.com", // 默认值
-					// 				require: true, // 必填字段 没有使用默认值(默认值得有)
-					// 			}
-					// 		},
-					// 	]
-					// },
->>>>>>> upload
-=======
-				   
->>>>>>> delete
 				}
 			}
 			wikiblock.init(initObj);
 			console.log($scope.params);
-
-
-
         }]);
     }
 
