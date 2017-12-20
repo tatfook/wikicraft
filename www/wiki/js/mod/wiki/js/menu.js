9ab1862b6240b9dfa227f966a36352ebd91100de
onedou <onedou@126.com>
2017-12-20 15:10:14 +0800

    indentation to space

--- www/wiki/js/mod/wiki/js/menu.js
+++ www/wiki/js/mod/wiki/js/menu.js
@@ -5,27 +5,27 @@
 define(['app', 'text!wikimod/wiki/html/menu.html'], function (app, htmlContent) {
     function registerController(wikiBlock) {
         app.registerController("menuController", ['$scope',function ($scope) {
-			wikiBlock.init({
-				scope:$scope,
-				styles:[
-				{
-					design:{
-						text:'left',
-					}
-				},
-				{
-					design:{
-						text:'right',
-					}
-				}
-				],
-				params_template:{
-					design:{
-						is_leaf: true,
-						require: true,
-						text:"left", // 默认值
-					},
-					menu:{
+            wikiBlock.init({
+                scope:$scope,
+                styles:[
+                {
+                    design:{
+                        text:'left',
+                    }
+                },
+                {
+                    design:{
+                        text:'right',
+                    }
+                }
+                ],
+                params_template:{
+                    design:{
+                        is_leaf: true,
+                        require: true,
+                        text:"left", // 默认值
+                    },
+                    menu:{
                         is_leaf: true,
                         type: "menu",
                         editable: true,
@@ -33,27 +33,27 @@ define(['app', 'text!wikimod/wiki/html/menu.html'], function (app, htmlContent) 
                         name: "菜单",
                         require: true,
                         text: [
-							{
-								id: 1,
-								name : '腾讯新闻',
-								url  : 'https://news.qq.com',
-								note : '腾讯新闻',
-								children: [
-									{
-										id   : 2,
-										name : '八卦新闻',
-										url  : 'https://news.qq.com',
-										note : '八卦新闻',
-										children: []
-									}
-								]
-							}
-						]
+                            {
+                                id: 1,
+                                name : '腾讯新闻',
+                                url  : 'https://news.qq.com',
+                                note : '腾讯新闻',
+                                children: [
+                                    {
+                                        id   : 2,
+                                        name : '八卦新闻',
+                                        url  : 'https://news.qq.com',
+                                        note : '八卦新闻',
+                                        children: []
+                                    }
+                                ]
+                            }
+                        ]
                     },
-				}
-			});
+                }
+            });
 
-			console.log($scope.params);			
+            console.log($scope.params);			
         }]);
     }
 
