9a9fab9d019b4286dd0b6c1ddde1fc32ed3ee11b
谭秉丰 <tanbingfeng@tanbingfengdeMac-mini.local>
2017-12-22 10:44:39 +0800

    old title

--- www/wiki/js/mod/wiki/js/title.js
+++ www/wiki/js/mod/wiki/js/title.js
@@ -1,69 +1,21 @@
-
 define([
     'app',
     'helper/util',
     'text!wikimod/wiki/html/title.html',
 ], function (app, util, htmlContent) {
-	var initObj;
 
+    // 使用闭包使模块重复独立使用
     function registerController(wikiblock) {
+        // 比赛类活动奖励控制器
         app.registerController("titleController", ['$scope','$sce', function ($scope, $sce) {
-			initObj = {
-				scope  : $scope,
-				styles : [
-					{
-						"design": {
-							"text":"style1",
-						},
-					},
-					{
-						"design": {
-							"text":"style2",
-						},
-					},
-				],
-				params_template : {
-					design:{
-						is_leaf: true, // 叶子对象默认填true
-						type:"text",   // 地段类型
-						editable:false, // 是否可以编辑
-						is_show:false,  // 可视化是否显示 undefined取值editable
-						name:"样式",   // 表单显示名
-						text:"style1", // 默认值
-						require: true, // 必填字段 没有使用默认值
-                    },
-                    img:{
-                        is_leaf: true, // 叶子对象默认填true
-                        type:"link",   // 地段类型
-                        editable:true, // 是否可以编辑
-                        is_show:true,  // 可视化是否显示 undefined取值editable
-                        name:"logo",   // 表单显示名
-                        text:config.wikiModPath + 'wiki/assets/imgs/titleMod.png', // 默认值
-                        href:config.wikiModPath + 'wiki/assets/imgs/titleMod.png', // 默认值
-                    	require: true, // 必填字段 没有使用默认值(默认值得有)
-                        },
-				   	hOne:{
-						is_leaf: true, // 叶子对象默认填true
-						type:"text",   // 地段类型
-						editable:true, // 是否可以编辑
-						is_show:true,  // 可视化是否显示 undefined取值editable
-						name:"标题",   // 表单显示名
-						text:"YOUR HEAD LINE", // 默认值
-						require: true, // 必填字段 没有使用默认值(默认值得有)
-					},
-				   	hTwo:{
-						is_leaf: true, // 叶子对象默认填true
-						type:"text",   // 地段类型
-						editable:true, // 是否可以编辑
-						is_show:true,  // 可视化是否显示 undefined取值editable
-						name:"子标题",   // 表单显示名
-						text:"YOUR SUB LINE", // 默认值
-						require: true, // 必填字段 没有使用默认值(默认值得有)
-					},
-				}
+            $scope.imgsPath = config.wikiModPath + 'wiki/assets/imgs/';
+            $scope.modParams = angular.copy(wikiblock.modParams || {});
+
+            function init() {
+                $scope.content = $sce.trustAsHtml(config.services.markdownit.render($scope.modParams.content || ""));
             }
 
-			wikiblock.init(initObj);
+            $scope.$watch("$viewContentLoaded", init);
         }]);
     }
 
@@ -71,10 +23,35 @@ define([
         render: function (wikiblock) {
             registerController(wikiblock);
             return htmlContent;
-		},
-		initObj: function(){
-			return initObj;
-		}
+        }
     }
 });
 
+/*
+```@wiki/js/title
+{
+    "moduleKind":"title",
+    "title":"大赛简介",
+    "content":"比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍"
+}
+```
+*/
+/*
+ ```@wiki/js/title
+ {
+ "moduleKind":"title2",
+ "column":"大赛简介",
+ "columnInfo":"大赛简介",
+ "content":"比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍比赛介绍"
+ }
+ ```
+ */
+/*
+ ```@wiki/js/title
+ {
+ "moduleKind":"title3",
+ "title":"标题标题",
+ "content":"简介简介  \n[可以加链接](链接url)  \n [可以加链接](链接Url)：（我不是链接）"
+ }
+ ```
+ */
