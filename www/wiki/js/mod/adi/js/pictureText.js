499af64848cbe077e7b25f0729285d7d05410f4d
谭秉丰 <tanbingfeng@tanbingfengdeMac-mini.local>
2017-12-29 20:37:01 +0800

    new picttureText

--- www/wiki/js/mod/adi/js/pictureText.js
+++ www/wiki/js/mod/adi/js/pictureText.js
@@ -33,7 +33,8 @@ define([
 					},
 					pictureTextImg:{
                         is_leaf: true, // 叶子对象默认填true
-                        type:"link",   // 地段类型
+						type:"media",   // 地段类型
+						mediaType: "image",
                         editable:true, // 是否可以编辑
                         is_mod_hide:false,  // 可视化是否显示 undefined取值editable
                         name:"picture",   // 表单显示名
