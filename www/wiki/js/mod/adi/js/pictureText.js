38c3ddaff58ccfcf6e48d24973f0608412eeaf13
onedou <onedou@126.com>
2018-01-03 23:14:16 +0800

    fix somethings

--- www/wiki/js/mod/adi/js/pictureText.js
+++ www/wiki/js/mod/adi/js/pictureText.js
@@ -7,6 +7,8 @@ define([
     function registerController(wikiblock) {
         app.registerController("pictureTextController", ['$scope','$sce', function ($scope, $sce) {
 
+			$scope.editorMode = wikiblock.editorMode;
+
             initObj = {
 				scope  : $scope,
 				styles : [
@@ -32,7 +34,7 @@ define([
 						text         : "style1",
 						require      : true,
 					},
-					pictureTextImg:{
+					image_picture:{
                         is_leaf      : true,
 						type         : "media",
 						mediaType    : "image",
@@ -44,7 +46,7 @@ define([
                         href         : config.wikiModPath + 'adi/assets/imgs/pictureMod.png',
                     	require      : true,
 					},
-					hOne:{
+					link_headline:{
 						is_leaf      : true,
 						type         : "link",
 						editable     : true,
@@ -55,7 +57,7 @@ define([
 						href         : "", 
 						require      : true,
 					},
-					hTwo:{
+					link_sublink:{
 						is_leaf      : true,
 						type         : "link",
 						editable     : true,
@@ -66,18 +68,7 @@ define([
 						href         : "",
 						require      : true,
 					},
-					spanOne:{
-						is_leaf      : true, 
-						type         : "link",
-						editable     : true,
-						is_card_show : true,
-            			is_mod_hide  : false,
-						name         : "文字说明",
-						text         : "",
-						href         : "",
-						require      : true,
-					},
-					btn:{
+					link_more:{
                         is_leaf      : true,
                         type         : "link",
                         editable     : true,
@@ -88,28 +79,35 @@ define([
                         href         : "",
                     	require      : true,
 					},
-					spanOne:{
+					multiText_desc:{
 						is_leaf      : true,
-						type         : "link",
+						type         : "multiText",
 						editable     : true,
 						is_card_show : true,
 						is_mod_hide  : false,
 						name         : "文字说明",
-						text         : "",
+						text         : $sce.trustAsHtml("&nbsp;&nbsp;&nbsp;&nbsp;一个人去旅行，而且是去故乡的山水间徜徉。\
+临行之前，面对太多的疑问和不解：为何是一个人？\
+也有善意的提醒：何不去远方！\
+昆明呀——赶一个花海；三亚呀——赴一个蓝天碧海。\
+只是微笑地固执自己的坚持，不做任何解释。\
+没有人明白，这一次是一个告别，或者一个永远的告别，以后我会去到很多很繁华或苍凉，辽远或偏僻的地方，而会常常想起这一次的旅行，想起那座山，那个城，那些人……\
+<br><br>\
+&nbsp;&nbsp;&nbsp;&nbsp;有时我们选择改变，并非经过深思熟虑，而更像是听见了天地间冥冥中的呼唤，呼唤你前往另一个地方，过上另一种生活。\
+你并不一定会从此拥有更美好的人生，可你仍然感谢天地和人世所带来的这些变化和发生。\
+不然你大概会一直好奇和不甘吧——家门前的那条小路，到底通向了什么样的远方呢？。\
+<br><br>\
+&nbsp;&nbsp;&nbsp;&nbsp;对于旅行，从来都记忆模糊。\
+记不得都去了哪些地方，看了哪些风景，遇到哪些人。\
+尽管同学说，去旅行不在于记忆，而在于当时的那份心情。\
+可是旅行的彼时那刻我的心情一直是好的吗？一直有记日记的习惯，可是，旅行回来，都懒得写日记来记录，可见内心底对旅行是多么的淡漠。\
+如果可以，我真想和你一直旅行。\
+或许是某个未开发的荒凉小岛，或许是某座闻名遐迩的文化古城。我们可以沿途用镜头记录彼此的笑脸，和属于我们的风景。\
+一起吃早餐，午餐，晚餐。或许吃得不好，可是却依旧为对方擦去嘴角的油渍。风景如何，其实并不重要。\
+重要的是，你在我的身边。"),
 						href         : "",
 						require      : true,
 					},
-					btn:{
-                        is_leaf      : true,
-                        type         : "link",
-						editable     : true,
-						is_card_show : true,
-                        is_mod_hide  : false,
-                        name         : "按钮", 
-                        text         : "更多照片",
-                        href         : "", 
-                    	require      : true,
-                    },
 				}
 			}
 
