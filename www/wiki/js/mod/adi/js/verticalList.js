72fcbedaef1c2c08b3de345d0dd75b82c516c184
谭秉丰 <tanbingfeng@tanbingfengdeMac-mini.local>
2018-01-05 17:35:49 +0800

    new vertica list js mod

--- www/wiki/js/mod/adi/js/verticalList.js
+++ www/wiki/js/mod/adi/js/verticalList.js
@@ -6,6 +6,7 @@ define([
 
     function registerController(wikiblock) {
         app.registerController("verticalListController", ['$scope','$sce', function ($scope, $sce) {
+			//竖排 list mod
 
 			$scope.editorMode = wikiblock.editorMode;
 
@@ -32,6 +33,75 @@ define([
 						is_mod_hide  : true,
 						name         : "样式",
 						text         : "style1",
+						require      : true,
+					},
+					image_imgOne: {
+						is_leaf      : true,
+						type         : "media",
+						mediaType    : "image",
+						editable     : true,
+						is_card_show : true,
+                        is_mod_hide  : false,
+                        name         : "图一",
+                        text         : config.wikiModPath + 'adi/assets/imgs/verticalList2.png',
+                        href         : '',
+                    	require      : true,
+					},
+					image_imgTwo: {
+						is_leaf      : true,
+						type         : "media",
+						mediaType    : "image",
+						editable     : true,
+						is_card_show : true,
+                        is_mod_hide  : false,
+                        name         : "图二",
+                        text         : config.wikiModPath + 'adi/assets/imgs/verticalList1.png',
+                        href         : '',
+                    	require      : true,
+					},
+					image_imgThree: {
+						is_leaf      : true,
+						type         : "media",
+						mediaType    : "image",
+						editable     : true,
+						is_card_show : true,
+                        is_mod_hide  : false,
+                        name         : "图三",
+                        text         : config.wikiModPath + 'adi/assets/imgs/verticalList3.png',
+                        href         : '',
+                    	require      : true,
+					},
+					multiText_decOne: {
+						is_leaf      : true,
+						type         : "multiText",
+						editable     : true,
+						is_card_show : true,
+						is_mod_hide  : false,
+						name         : "文字说明",
+						text         : '',
+						href         : "",
+						require      : true,
+					},
+					multiText_decTwo: {
+						is_leaf      : true,
+						type         : "multiText",
+						editable     : true,
+						is_card_show : true,
+						is_mod_hide  : false,
+						name         : "文字说明",
+						text         : '',
+						href         : "",
+						require      : true,
+					},
+					multiText_decThree: {
+						is_leaf      : true,
+						type         : "multiText",
+						editable     : true,
+						is_card_show : true,
+						is_mod_hide  : false,
+						name         : "文字说明",
+						text         : '',
+						href         : "",
 						require      : true,
 					},
 				}
