52104fe4b0461a79fe72d0243f06b2d6e1e213ef
谭秉丰 <tanbingfeng@tanbingfengdeMac-mini.local>
2018-01-09 11:15:14 +0800

    new spaces

--- www/wiki/js/mod/adi/js/photo.js
+++ www/wiki/js/mod/adi/js/photo.js
@@ -6,55 +6,55 @@ define([
 
     function registerController(wikiblock) {
         app.registerController("photoController", ['$scope','$sce', function ($scope, $sce) {
-			$scope.editorMode = wikiblock.editorMode;
+            $scope.editorMode = wikiblock.editorMode;
 
-			initObj = {
-				scope  : $scope,
-				styles : [
-					{
-						"design": {
-							"text":"style1",
-						},
-					},
-				],
-				params_template : {
-					design:{
-						is_leaf      : true, 
-						type         : "text",   
-						editable     : false, 
-						is_mod_hide  : true,  
-						is_card_show : true,
-						name         : "样式",   
-						text         : "style1", 
-						require      : true, 
+            initObj = {
+                scope  : $scope,
+                styles : [
+                    {
+                        "design": {
+                            "text":"style1",
+                        },
+                    },
+                ],
+                params_template : {
+                    design:{
+                        is_leaf      : true, 
+                        type         : "text",   
+                        editable     : false, 
+                        is_mod_hide  : true,  
+                        is_card_show : true,
+                        name         : "样式",   
+                        text         : "style1", 
+                        require      : true, 
                     },
                     media_photo:{
                         is_leaf      : true, 
-						type         : "media",   
-						mediaType    : "image",
+                        type         : "media",   
+                        mediaType    : "image",
                         editable     : true, 
-						is_mod_hide  : true,  
-						is_card_show : true,
+                        is_mod_hide  : true,  
+                        is_card_show : true,
                         name         : "photo",   
                         text         : config.wikiModPath + 'adi/assets/imgs/photo.png', 
                         href         : "", 
-                    	require      : true, 
+                        require      : true, 
                         },
-				}
+                }
             }
 
-			wikiblock.init(initObj);
-		}]);
+            wikiblock.init(initObj);
+        }]);
     }
 
     return {
         render: function (wikiblock) {
             registerController(wikiblock);
             return htmlContent;
-		},
-		initObj: function(){
-			return initObj;
-		}
+        },
+        initObj: function(){
+            return initObj;
+        }
     }
 });
 
