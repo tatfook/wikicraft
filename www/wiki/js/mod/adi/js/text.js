f3dc42463aacf7f3d929517fb6cfef00dd1d8451
oushifa <ukinll@163.com>
2018-01-10 17:47:06 +0800

    fixed multi_text href

--- www/wiki/js/mod/adi/js/text.js
+++ www/wiki/js/mod/adi/js/text.js
@@ -53,6 +53,8 @@ define([
                         name         : "文字说明",
                         text         : '一个人去旅行，而且是去故乡的山水间徜徉。临行之前，面对太多的疑问和不解：为何是一个人？也有善意的提醒：何不去远方！昆明呀——赶一个花海；三亚呀——赴一个蓝天碧海。只是微笑地固执自己的坚持，不做任何解释。没有人明白，这一次是一个告别，或者一个永远的告别，以后我会去到很多很繁华或苍凉，辽远或偏僻的地方，而会常常想起这一次的旅行，想起那座山，那个城，那些人……',
                         require      : true,
+                        href: '',
+                        target:'_blank'
 					}
                 }
             })
@@ -61,6 +63,7 @@ define([
                 langPrefix: 'code-',
             })
             $scope.params.multiText_desc.text = md.render($scope.params.multiText_desc.text);
+            $scope.targetIf = $scope.params.multiText_desc.href.length == 0
         }])
     }
     
