b8b411bfb4168ec4e86d4e4368a4c47ba44833bd
onedou <onedou@126.com>
2018-01-12 14:42:35 +0800

    fix multi text

--- www/wiki/js/mod/adi/js/project.js
+++ www/wiki/js/mod/adi/js/project.js
@@ -128,7 +128,10 @@ define([
 				langPrefix: 'code-'
 			})
 			
-			$scope.params.multiText_desc.text = md.render($scope.params.multiText_desc.text);
+			$scope.$watch('params', function(){
+				$scope.multiText_desc_md = md.render($scope.params.multiText_desc.text);
+			})
+
 			$scope.projectImg = {
 				"background-image"    : 'url('+ $scope.params.image_picture.text +')',
 				"background-size"     : "cover",
