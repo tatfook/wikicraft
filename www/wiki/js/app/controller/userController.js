01c7c85639a30eed56ca59387b8eee43f2fa81e5
ZhangKaitlyn <1873070425@qq.com>
2018-01-16 19:32:49 +0800

    个人主页改版部分

--- www/wiki/js/app/controller/userController.js
+++ www/wiki/js/app/controller/userController.js
@@ -12,190 +12,40 @@ define([
     //console.log("load userController file");
 
     app.registerController('userController', ['$scope','Account','Message', 'modal', function ($scope, Account, Message, modal) {
+        
+    }]);
+
+    app.registerController('userMsgCtrl', ['$scope', 'Account', function($scope, Account) {
+        $scope.userinfo = {
+            portrait: "http://git.keepwork.com/gitlab_rls_kaitlyn/keepworkdatasource/raw/master/kaitlyn_images/img_1516090899563.png",
+            displayName: "username",
+            username:"username",
+            joindate: "2016-09-12",
+            location: "深圳",
+            email:"8743927@outlook.com",
+            worksCount: 4,
+            favoriteCount: 12,
+            fansCount: 35,
+        };
         function init(userinfo) {
-            var username = $scope.urlObj.username;
+            var username = $scope.urlObj.username.toLowerCase();;
             if (!username && userinfo && userinfo.username) {
                 username = userinfo.username;
             }
             if (!username) {
-				util.go("home");
+                console.error("用户名不存在");
                 return;
             }
-			username = username.toLowerCase();
+
             util.post(config.apiUrlPrefix + 'user/getDetailByName', {username:username}, function (data) {
                 if (!data) {
-					util.go("home");
+                    console.error("用户信息不存在");
                     return ;
                 }
                 // 用户信息
-                $scope.userinfo = data.userinfo;
-                $scope.selfOrganizationList = data.selfOrganizationObj.siteList;
-                $scope.selfOrganizationCount = data.selfOrganizationObj.siteList.length;
-                $scope.joinOrganizationList = data.joinOrganizationObj.siteList;
-                $scope.joinOrganizationCount = data.joinOrganizationObj.siteList.length;
-                $scope.hotSiteList = data.hotSiteObj.siteList;
-                $scope.hotSiteTotal=data.hotSiteObj.siteList.length;
-                $scope.allSiteList = data.allSiteList;
-                $scope.allSiteTotal = data.allSiteList.length;
-                // 粉丝
-                $scope.fansList = data.fansObj.userList;
-                $scope.fansCount = data.fansObj.total;
-                // 关注的用户
-                $scope.followUserList = data.followObj.followUserObj.userList;
-                $scope.followUserTotal = data.followObj.followUserObj.total;
-                // 关注的站点
-                $scope.followSiteList = data.followObj.followSiteObj.siteList;
-                $scope.followSiteTotal = data.followObj.followSiteObj.total;
-                // 用户动态
-                $scope.trendsList = data.trendsObj.trendsList;
-                $scope.trendsCount = data.trendsObj.total;
-                $scope.active = data.activeObj;
-                if(data.activeObj){
-                    data.activeObj.before="calendarSibling";//插入在某个子元素的前面，默认在子元素的尾部，
-                    contributionCalendar("contributeCalendar",data.activeObj);
-                }else{
-                    contributionCalendar("contributeCalendar",{before:"calendarSibling"});
-                }
-
-                if ($scope.user && $scope.user._id) {
-                    util.post(config.apiUrlPrefix + "user_fans/isAttented", {userId:$scope.userinfo._id, fansUserId:$scope.user._id}, function (data) {
-                        $scope.concerned = data;
-                        $scope.userinfo.concerned=data;
-                    });
-                }
-            });
-
-            // 获取参与的网站
-            util.post(config.apiUrlPrefix + "site_user/getSiteListByMemberName", {
-                memberName: username,
-            }, function(data){
-                $scope.joinSiteList = [];
-                $scope.joinSiteTotal = 0;
-                data = data || [];
-                for (var i = data.length - 1; i>=0; i--){
-                    if (data[i].siteinfo){
-                        $scope.joinSiteList.push(data[i].siteinfo);
-                        $scope.joinSiteTotal++;
-                    }
-                }
-            }, function (err) {
-                console.log(err);
+                // $scope.userinfo = data.userinfo;
             });
         }
-
-        $scope.favoriteUser = function (fansUser, subInfo) {
-            if (!$scope.userinfo) {
-                $scope.concerned = !$scope.concerned;
-                return;
-            }
-
-            if (!Account.isAuthenticated()) {
-                Message.info("登录后才能关注");
-                modal('controller/loginController', {
-                    controller: 'loginController',
-                    size: 'lg',
-                    backdrop: true
-                }, function (result) {
-                    console.log(result);
-                    // nowPage.replaceSelection(login.content);
-                }, function (result) {
-                    console.log(result);
-                });
-                return; // 登录后才能关注
-            }
-
-            fansUser = fansUser ? fansUser : $scope.userinfo;//关注该页面的用户，或者关注这个用户的粉丝
-
-            if (!Account.isAuthenticated() || !$scope.user || $scope.user._id == fansUser._id) {
-                Message.info("自己不关注自己");
-                return; // 自己不关注自己
-            }
-
-            var ownUserFan = {
-                "userId": fansUser._id,
-                "fansUserId": $scope.user._id,
-                "userinfo": $scope.user
-            };
-
-            if(fansUser.concerned){//取消关注
-                util.post(config.apiUrlPrefix + 'user_fans/unattent', {userId:fansUser._id, fansUserId:$scope.user._id}, function () {
-                    console.log("取消关注成功");
-                    Message.info("取消关注成功");
-                    fansUser.concerned=false;
-                    if (subInfo && subInfo == "fansOpt"){
-                        $.each($scope.fansList, function (index, fansItem) {
-                            if (fansItem.fansUserId == $scope.user._id){
-                                $scope.fansList[index].isDelete = true;
-                            }
-                        });
-                        $scope.fansCount--;
-                    }
-                });
-            }else{
-                util.post(config.apiUrlPrefix + 'user_fans/attent', {userId:fansUser._id, fansUserId:$scope.user._id}, function () {
-                    console.log("关注成功");
-                    Message.info("关注成功");
-                    fansUser.concerned=true;
-                    if (subInfo && subInfo == "fansOpt"){
-                        $scope.fansCount++;
-                        $scope.fansList.push(ownUserFan);
-                    }
-                });
-            }
-        };
-        
-        $scope.isShowNavBar = function () {
-            if ($scope.user && $scope.userinfo && $scope.user.username == $scope.userinfo.username) {
-                return true;
-            }
-            return false;
-        };
-
-        $scope.loadActivity = function () {
-            Message.info("暂无更多活动");
-        };
-
-        $scope.goUserSite = function (x) {
-            util.goUserSite('/' + x.username + '/' + x.name, true);
-        };
-
-        $scope.goUserIndexPage = function (name) {
-            util.go("/"+name);
-        };
-        $scope.goHelpPage = function () {
-            util.go("knowledge");
-        };
-
-        $scope.goNewWebsitePage = function () {
-            storage.sessionStorageSetItem('userCenterContentType', "newWebsite");
-            util.go("userCenter");
-        };
-        
-        $scope.goWebsitePage = function () {
-            storage.sessionStorageSetItem('userCenterContentType', "websiteManager");
-            util.go("userCenter");
-        };
-        
-        $scope.goEditorPage = function () {
-            util.go("wikieditor");
-        };
-
-        // 退出组织
-        $scope.exitOrg = function (organization) {
-            console.log(organization);
-            config.services.confirmDialog({
-                "title": "删除提醒",
-                "theme": "danger",
-                "confirmBtnClass": "btn-danger",
-                "content": "确定退出 " + organization.siteinfo.name + " 组织？"
-            },function () {
-                util.post(config.apiUrlPrefix + 'website_member/deleteById', organization, function () {
-                    organization.isDelete = true;
-                    $scope.joinOrganizationCount--;
-                });
-            });
-        };
-
         $scope.$watch('$viewContentLoaded', function () {
             //console.log("------------------init user controller----------------------");
             if ($scope.urlObj.username) {
@@ -206,6 +56,29 @@ define([
                 }
             }
         });
+    }]);
+
+    app.registerController('worksCtrl', ['$scope', function($scope){
+        $scope.works = [
+            {
+                logoUrl:"http://git.keepwork.com/gitlab_rls_kaitlyn/keepworkdatasource/raw/master/kaitlyn_images/img_1516099391929.jpeg",
+                title:"日常摄影作品",
+                workLink:"/photograph/page01",
+                desc:"介绍日常生活中容易拍摄的照片",
+                tags:["摄影","任务","记录"],
+                visitCount:253,
+                favoriteCount:43
+            },
+            {
+                logoUrl:"http://git.keepwork.com/gitlab_rls_kaitlyn/keepworkdatasource/raw/master/kaitlyn_images/img_1516099391929.jpeg",
+                title:"code",
+                workLink:"/photograph/page01",
+                desc:"介绍日常生活中容易拍摄的照片",
+                tags:["编程","ui"],
+                visitCount:253,
+                favoriteCount:43
+            }
+        ];
     }]);
 
     return htmlContent;
