11e355bd9e7f79f399a7c934fd44a50a19b4ce09
Tpp <307926882>
2018-01-16 18:15:57 +0800

    new paratext

--- www/wiki/js/mod/adi/js/paratext.js
+++ www/wiki/js/mod/adi/js/paratext.js
@@ -1,61 +1,32 @@
 define([
 	'app',
-	'markdown-it',
     'helper/util',
 	'text!wikimod/adi/html/paratext.html',
-], function (app, markdown_it, util, htmlContent) {
-
+    'helper/mdconf',
+], function (app, util, htmlContent, mdconf) {
     function registerController(wikiblock) {
-        app.registerController("paratextController", ['$scope','$sce', function ($scope, $sce) {
-
+        app.registerController("paratextController", ['$scope', '$uibModal', '$sce', function ($scope, $uibModal, $sce) {
 			$scope.editorMode = wikiblock.editorMode;
-
-            initObj = {
-				scope  : $scope,
-				styles : [
-					{
-						"design": {
-                            "text":"style1",
-						},
+			wikiblock.init({
+                scope  : $scope,
+				styles : [],
+				params_template : {
+                    paratext_desc:{
+                        is_leaf      : true,
+						type         : "paratext",
+                        editable     : true,
+						is_card_show : true,
+						is_mod_hide  : false,
+                        name         : "富文本",
+                        svg          : "",
+                        compressData : "",
+                    	require      : true,
                     },
-				],
-				params_template: {
-					design:{
-						is_leaf      : true,
-						type         : "text",
-						editable     : false,
-						is_card_show : true,
-						is_mod_hide  : false,
-						name         : "样式",
-						text         : "style1",
-						require      : true,
-					},
-					multiText_desc:{
-						is_leaf      : true,
-						type         : "multiText",
-						editable     : true,
-						is_card_show : true,
-						is_mod_hide  : false,
-						name         : "文字说明",
-						text         : "123",
-						href         : "",
-						require      : true,
-					},
-				}
-			}
-
-			
-			wikiblock.init(initObj);
-
-			var md = new markdown_it({
-				html: true,
-				langPrefix: 'code-'
-			})
-			
-			$scope.$watch('params', function(){
-				$scope.multiText_desc_md = md.render($scope.params.multiText_desc.text);
-			})
-
+                }
+			});
+		}])
+		app.registerController("paratextEditorController", ['$scope', '$uibModalInstance', 'wikiBlock', function ($scope, $uibModalInstance, wikiBlock) {
+           
         }]);
     }
 
