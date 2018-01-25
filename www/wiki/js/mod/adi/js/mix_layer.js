d408ab5900b3eb8ab582ec2fe88f4690eacb5acc
Tpp <307926882>
2018-01-25 18:10:12 +0800

    new

--- www/wiki/js/mod/adi/js/mix_layer.js
+++ www/wiki/js/mod/adi/js/mix_layer.js
@@ -8,6 +8,7 @@ define([
     function registerController(wikiblock) {
         app.registerController("mix_layerController", ['$scope','$sce', function ($scope, $sce) {
             $scope.editorMode = wikiblock.editorMode;
+            var mixDesc = "加利福尼亚大学伯克利分校是美国最负盛名且是最顶尖的一所公立研究型大学，位于旧金山东湾伯克利市的山丘上。1868年由加利福尼亚学院以及农业、矿业和机械学院合并而成，1873年迁至圣弗朗西斯科（旧金山）附近的伯克利市。伯克利加大是加利福尼亚大学中最老的一所。它也是美国大学协会（Association of American Universities）创始会员之一。其吉祥物蜕变自加州徽号，故其学生亦常自称“金色小熊”。加州大学伯克利分校与斯坦福大学、麻省理工学院等一同被誉为美国工程科技界的学术领袖。"
 
             initObj = {
 				scope  : $scope,
@@ -132,12 +133,47 @@ define([
             var md = new markdown_it({
 				html: true,
 				langPrefix: 'code-'
-			})
+            })          
             
+            // $scope.mixImgHeight = function(params){
+            //     var setHeightInterval = setInterval(function(params){
+            //         var miximg = document.querySelector('#' + $scope.imgHeight);
+            //         var mixdiv = document.querySelector('#' + $scope.divHeight);
+            //         if($scope.params.multiText_content.text.length > mixDesc.length){
+            //             if(miximg.offsetHeight < 1739){
+            //                 miximg.style.height = mixdiv.offsetHeight + 'px';
+            //             }else{
+            //                 mixdiv.style.marginTop = 700 + 'px';
+            //                 miximg.style.height    = 694 + 'px';
+            //             }
+            //         }
+            //     }, 100); 
+            // }
+
+
+            var setHeightInterval = setInterval(function(params){
+                var miximg = document.querySelector('#' + $scope.imgHeight);
+                var mixdiv = document.querySelector('#' + $scope.divHeight);
+                if($scope.params.multiText_content.text.length > mixDesc.length){
+                    if(miximg.offsetHeight < 1739){
+                        miximg.style.height = mixdiv.offsetHeight + 'px';
+                    }else{
+                        mixdiv.style.marginTop = 700 + 'px';
+                        miximg.style.height    = 694 + 'px';
+                    }
+                }
+            }, 100); 
 
 			$scope.$watch('params', function(){
-				$scope.multiText_content = md.render($scope.params.multiText_content.text);
-			})
+                $scope.multiText_content = md.render($scope.params.multiText_content.text);
+            })
+            
+            // if($scope.params.multiText_content.text.length > mixDesc.length){
+            //     $scope.$watch("$viewContentLoaded", $scope.mixImgHeight);
+            // }
+
+            $scope.imgHeight = "__MIXIMG__" + Date.now();
+            $scope.divHeight = "__MIXDIV__" + Date.now();
         }]);
     }
 
