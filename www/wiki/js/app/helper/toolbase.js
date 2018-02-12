26ace1a25daf8f8cbeb8c7e4a6a2a329ebc6e84d
xiaoyao <765485868@qq.com>
2018-02-12 21:52:13 +0800

    fix event

--- www/wiki/js/app/helper/toolbase.js
+++ www/wiki/js/app/helper/toolbase.js
@@ -1,83 +1,36 @@
+ //xiaoyao 2018-02-12
+//toolbase 提供事件机制, 事件机制可以绑定到对象上使用， 也可全局使用
+
+//作为基类 绑定到对象上使用
+//sender = Object.assign(sender, toolbase);
+//sender.addEventListener("eventName", function(){
+	//console.log("sender trigger eventName");
+//});
+//sender.dispatchEvent("eventName", "event params");
+//全局使用
+//toolbase.addEventListener("eventName", function(){
+	//console.log("toolbase trigger eventName");
+//});
+//toolbase.dispatchEvent("eventName", "event params");
+
 
 define([
 	"app",
 ], function(app) {
 	// 事件机制可绑定到单一对象上使用 也可全局使用
-	var eventMap = {};
 	var toolbase = {};
-	
-	// 判断对象是有具有事件能力
-	toolbase.isEventObject = function(){
-		if (this.getEventMap) { // 使用此方法判定是否具有事件能力
-			return true;
-		}
-
-		return false;
-	}
-	// 定义添加事件方法
-	toolbase.defineEvent = function(eventName, eventObj) {
-		var self = eventObj || this;
-		var eventMap = self.getEventMap ? self.getEventMap() : {};
-
-		if (!eventName) {
-			return;
-		}
-
-		// 定义添加事件方法
-		self.defineEvent = self.defineEvent || function(eventName) {
-			toolbase.defineEvent(eventName, self);
-		}
-		
-		// 移除事件
-		self.undefineEvent = self.undefineEvent || function(eventName) {
-			toolbase.undefineEvent(eventName, self);
-		}
-
-		// 定义触发信号函数
-		self.dispatchEvent = self.dispatchEvent || function(eventName, params){
-			toolbase.dispatch(eventName, params, self);
-		};
-
-		// 定义监听事件接口
-		self.addEventListener = self.addEventListener || function(eventName, handler, funcname) {
-			toolbase.addEventListener(eventName, handler, funcname, self);
-		}
-
-		// 定义监听事件接口
-		self.removeEventListener = self.removeEventListener || function(eventName, handler, funcname) {
-			toolbase.removeEventListener(eventName, handler, funcname, self);
-		}
-
-		// 定义获取所有信号
-		self.getEventMap = self.getEventMap || function(){
-			return eventMap;
-		}
-
-		eventMap[eventName] = {eventName:eventName, eventHandles:[]};
-	}
-
-	// 移除事件
-	toolbase.undefineEvent = function(eventName, eventObj) {
-		var self = eventObj || this;
-		var eventMap = self.getEventMap ? self.getEventMap() : {};
-
-		delete eventMap[eventName];
-	}
 
 	// 定义触发信号函数
-	toolbase.dispatchEvent = function(eventName, params, eventObj) {
-		var self = eventObj || this;
-
-		!self.isEventObject() && self.defineEvent(eventName);
-
-		var eventMap = self.getEventMap ? self.getEventMap() : {};
+	toolbase.dispatchEvent = function(eventName, params) {
+		var self = this;
+		var _event = self.getEvent(eventName);
 
 		// typeof(handler) == "function" or typeof(handler[eventName]) == "function"
-		if (!eventName || !eventMap[eventName]) {
+		if (!_event) {
 			return;
 		}
 		
-		var eventHandles = eventMap[eventName].eventHandles;
+		var eventHandles = _event.eventHandles;
 		for (var i = 0; i < eventHandles.length; i++) {
 			var handle = eventHandles[i];
 			var handler = handle.handler;
@@ -92,19 +45,16 @@ define([
 	}
 
 	// 定义添加监听事件接口
-	toolbase.addEventListener = function (eventName, handler, funcname, eventObj) {
-		var self = eventObj || this;
-
-		!self.isEventObject() && self.defineEvent(eventName);
-
-		var eventMap = self.getEventMap ? self.getEventMap() : {};
+	toolbase.addEventListener = function (eventName, handler, funcname) {
+		var self = this;
+		var _event = self.getEvent(eventName);
 
 		// typeof(handler) == "function" or typeof(handler[eventName]) == "function"
-		if (!eventName || !eventMap[eventName] || !handler) {
+		if (!_event || !handler) {
 			return;
 		}
 		
-		var eventHandles = eventMap[eventName].eventHandles;
+		var eventHandles = _event.eventHandles;
 		var handle = {handler: handler, funcname:funcname};
 		// 禁止同一处理程序重复监听  外部实现重复监听比不重复要简单 且不重复更广泛(经验值判定)  故内部屏蔽
 		for (var i = 0; i < eventHandles.length; i++) {
@@ -118,19 +68,16 @@ define([
 	}
 	
 	// 定义删除监听事件接口
-	toolbase.removeEventListener = function (eventName, handler, funcname, eventObj) {
-		var self = eventObj || this;
-		!self.isEventObject() && self.defineEvent(eventName);
+	toolbase.removeEventListener = function (eventName, handler, funcname) {
+		var self = this;
+		var _event = self.getEvent(eventName);
 
-		var eventMap = self.getEventMap ? self.getEventMap() : {};
-
-		// typeof(handler) == "function" or typeof(handler[eventName]) == "function"
-		if (!eventName || !eventMap[eventName]) {
+		if (!_event) {
 			return;
 		}
 
 		var newEventHandles = [];
-		var eventHandles = eventMap[eventName].eventHandles;
+		var eventHandles = _event.eventHandles;
 		// 禁止同一处理程序重复监听  外部实现重复监听比不重复要简单 且不重复更广泛(经验值判定)  故内部屏蔽
 		for (var i = 0; i < eventHandles.length; i++) {
 			// 删除该类型所有事件
@@ -142,19 +89,34 @@ define([
 				continue;
 			}
 			// 删除指定事件
-			if (eventHandles[i].handler == handler && eventHandles[i].funcname == funcname)){
+			if (eventHandles[i].handler == handler && eventHandles[i].funcname == funcname){
 				continue
 			}
 
 			newEventHandles.push(eventHandles[i]);
 		}
 
-		eventMap[eventName].eventHandles = newEventHandles;
+		_event.eventHandles = newEventHandles;
 	}
 
 	// 定义获取所有信号
+	toolbase.getEvent = function(eventName){
+		var eventMap = this.getEventMap();
+		if (!eventName) {
+			return undefined;
+		}
+
+		eventMap[eventName] = eventMap[eventName] || {eventName:eventName, eventHandles:[]};
+		return eventMap[eventName];
+	}
+
 	toolbase.getEventMap = function(){
-		return eventMap;
+		this.$eventMap = this.$eventMap || {$event:this};
+		if (this.$eventMap.$event != this) {
+			this.$eventMap = {$event:this};
+		}
+
+		return this.$eventMap;
 	}
 
 	return toolbase;
