d5580daf1e0d8a89525dc264e13376df9b09e97e
onedou <onedou@126.com>
2018-09-26 11:45:00 +0800

    1. recharge js logic 2. add thumbnail field 3. update goods and knowledgeBean api

--- www/wiki/js/mod/knowledgeBean/index.js
+++ www/wiki/js/mod/knowledgeBean/index.js
@@ -18,14 +18,79 @@
   app.registerController('knowledgeBeanController', [
     '$scope',
     function($scope) {
+      $scope.RECHARGEPAGE = 'RECHARGEPAGE'
+      $scope.LOADINGPAGE = 'LOADINGPAGE'
+
+      $scope.page = $scope.RECHARGEPAGE
+      $scope.myKnowledgeBean = 300
+      $scope.spendKnowledgeBean = 21
+      $scope.goodsList = []
+
       $scope.recharge = function() {
-        console.log(baseUrl)
-        // util.post()
+        // $scope.page = $scope.LOADINGPAGE
+        console.log($scope.goodsList)
       }
 
       $scope.getGoodsList = function() {
-        return [{},{}]
+        var url = baseUrl + '/getGoodsList'
+
+        function handleGoodsList(data) {
+          if (Array.isArray(data)) {
+            $scope.goodsList = data
+          }
+        }
+
+        util.post(url, {}, handleGoodsList, function() {}, false)
       }
+
+      $scope.getCurrentBuyCount = function(item) {
+        return item.buyCount || 0
+      }
+
+      $scope.minus = function(item) {
+        if (!item.buyCount || typeof(item.buyCount) != 'number') {
+          item.buyCount = 0
+        } else {
+          if (item.buyCount < 1) {
+            item.buyCount = 0
+          } else {
+            item.buyCount = item.buyCount - 1
+          }
+        }
+      }
+
+      $scope.plus = function(item) {
+        if (!item.buyCount || typeof(item.buyCount) != 'number') {
+          item.buyCount = 1
+        } else {
+          item.buyCount = item.buyCount + 1
+        }
+      }
+
+      $scope.getSpendKnowledgeBean = function() {
+        var spendKnowledgeBean = 0
+
+        for (var x in $scope.goodsList) {
+          var currentGoods = $scope.goodsList[x]
+
+          if(currentGoods && currentGoods.bean && currentGoods.buyCount) {
+            spendKnowledgeBean = spendKnowledgeBean + (currentGoods.buyCount * currentGoods.bean)
+          }
+        }
+
+        return spendKnowledgeBean
+      }
+
+      function init() {
+        var queryArgs = util.getQueryObject()
+
+        $scope.username = queryArgs.username || ''
+        $scope.haqiNum = queryArgs.haqiNum || ''
+
+        $scope.getGoodsList()
+      }
+
+      $scope.$watch('$viewContentLoaded', init)
     }
   ])
 
